924baea6ad9577bc7ae955436cb57dd7432c17c4
tracer-ysyx <tracer@ysyx.org>
2025-04-10 20:14:09 +0800

    > sim RTL ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:09 up  5:30,  1 user,  load average: 0.29, 0.41, 0.35

--- pre_study/common.mk
+++ pre_study/common.mk
@@ -43,7 +43,7 @@ run: $(BIN)
 	@$^
 
 # 上面是NVBOARD的内容
-VERILATOR_SIM_CFLAGS += -Wall --cc -exe --build --trace \
+VERILATOR_SIM_CFLAGS += -Wall --cc -exe --build --trace-fst \
 						 vsrc/$(TOPNAME).v csrc/main.cpp
 VERILATOR_EXE_CFLAGS +=
 
