72ffe9eaf8e47ab2972a5c86e0d7a6f852e953ce
Xie Pingjiang <2690477553@qq.com>
2025-04-13 10:23:40 +0800

    第三次

---
+++ pre_study/alu/obj_dir/Vtop__ALL.d
@@ -0,0 +1,18 @@
+Vtop__ALL.o: Vtop__ALL.cpp Vtop.cpp Vtop.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilatedos.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_config.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_types.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_funcs.h \
+ Vtop__Syms.h Vtop___024root.h Vtop___024unit.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_fst_c.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_trace.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_trace_defs.h \
+ /home/yunhai/tools/verilator/share/verilator/include/gtkwave/fstapi.h \
+ Vtop___024root__DepSet_h84412442__0.cpp \
+ Vtop___024root__DepSet_heccd7ead__0.cpp Vtop__Trace__0.cpp \
+ Vtop___024root__Slow.cpp Vtop___024root__DepSet_h84412442__0__Slow.cpp \
+ Vtop___024root__DepSet_heccd7ead__0__Slow.cpp Vtop___024unit__Slow.cpp \
+ Vtop___024unit__DepSet_hff17caec__0__Slow.cpp Vtop__Syms.cpp \
+ Vtop__Trace__0__Slow.cpp
---
+++ pre_study/led/build/obj_dir/Vtop__ALL.d
@@ -0,0 +1,9 @@
+Vtop__ALL.o: Vtop__ALL.cpp Vtop.cpp Vtop.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilatedos.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_config.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_types.h \
+ /home/yunhai/tools/verilator/share/verilator/include/verilated_funcs.h \
+ Vtop__Syms.h Vtop___024root.h Vtop___024root__DepSet_h84412442__0.cpp \
+ Vtop___024root__DepSet_heccd7ead__0.cpp Vtop___024root__Slow.cpp \
+ Vtop___024root__DepSet_heccd7ead__0__Slow.cpp Vtop__Syms.cpp
