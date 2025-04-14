ce2b62557c08a48cfe75c9ec65a0f3390aa84a65
tracer-ysyx <tracer@ysyx.org>
2025-04-14 11:04:01 +0800

    > sim RTL ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:04:01 up  2:01,  1 user,  load average: 1.68, 1.47, 1.12

---
+++ pre_study/alu/obj_dir/main.d
@@ -0,0 +1,9 @@
+main.o: ../csrc/main.cpp Vtop.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilatedos.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_config.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_types.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_funcs.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_fst_c.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated.h \
+ /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_trace.h
