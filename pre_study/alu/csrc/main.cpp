2095ae515b849b41a1cdd90b7966d5dbd2695913
tracer-ysyx <tracer@ysyx.org>
2025-04-14 11:29:34 +0800

    > sim RTL ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:34 up  2:27,  1 user,  load average: 1.83, 1.11, 0.97

--- pre_study/alu/csrc/main.cpp
+++ pre_study/alu/csrc/main.cpp
@@ -80,7 +80,7 @@
 			top->B_i = rand()%16-8;
 			top->op_i = 0;
 			top->eval();
-			Y = (top->out_o > 7) ? top->out_o : 16-(int8_t)top->out_o;
+			Y = (top->out_o < 7) ? top->out_o : 16-(int8_t)top->out_o;
 			printf("A = %d, B = %d, Y = %d\n", (int8_t)top->A_i, (int8_t)top->B_i, Y);
 			check_out_valid(top);
 			tfp->dump(sim_time);
