262d3965e8bac00341505a3f54454cf6fb152707
tracer-ysyx <tracer@ysyx.org>
2025-04-14 11:32:15 +0800

    > sim RTL ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:15 up  2:29,  1 user,  load average: 1.05, 1.02, 0.96

--- pre_study/alu/csrc/main.cpp
+++ pre_study/alu/csrc/main.cpp
@@ -52,7 +52,8 @@
 			break;
 		}
 		printf("Y = %d, zero = %d, overflow_o = %d, carry_o = %d\n",Y,zero,overflow,carry);
-		assert(((dut->out_o < 7) ? dut->out_o : (int8_t)dut->out_o-16) == Y);
+		int8_t Y00 = (top->out_o < 7) ? top->out_o : (int8_t)top->out_o-16;
+		assert(Y00 == Y);
 		assert(dut->zero_o == zero);
 		assert(dut->overflow_o == overflow);
 		assert(dut->carry_o == carry);
