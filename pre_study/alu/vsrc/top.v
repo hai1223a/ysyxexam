e60e215045d28c2cbbd6e7f62beb2aca0dd275ce
tracer-ysyx <tracer@ysyx.org>
2025-04-14 11:03:46 +0800

    > sim RTL ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:46 up  2:01,  1 user,  load average: 1.60, 1.44, 1.11

--- pre_study/alu/vsrc/top.v
+++ pre_study/alu/vsrc/top.v
@@ -1,68 +1,47 @@
-/****** alu.sv ******/
-typedef enum logic [1:0] {
-     add     = 2'h1,
-     sub     = 2'h2,
-     nop     = 2'h0
-} operation_t /*verilator public*/;
-// 上面这个注释很关键，由他负责转换这个为C++
-
-module top #(
-        parameter WIDTH = 6
-) (
-        input clk,
-        input rst,
-
-        input  operation_t  op_in,
-        input  [WIDTH-1:0]  a_in,
-        input  [WIDTH-1:0]  b_in,
-        input               in_valid,
-
-        output logic [WIDTH-1:0]  out,
-        output logic              out_valid
+module top(
+  input [2:0] op_i,
+  input [3:0] A_i,
+  input [3:0] B_i,
+  output reg [3:0] out_o,
+  output reg zero_o,
+  output reg overflow_o,
+  output reg carry_o
 );
-
-        operation_t  op_in_r;
-        logic  [WIDTH-1:0]  a_in_r;
-        logic  [WIDTH-1:0]  b_in_r;
-        logic               in_valid_r;
-        logic  [WIDTH-1:0]  result;
-
-        // Register all inputs
-        always_ff @ (posedge clk, posedge rst) begin
-                if (rst) begin
-                        op_in_r     <= nop;
-                        a_in_r      <= '0;
-                        b_in_r      <= '0;
-                        in_valid_r  <= '0;
-                end else begin
-                        op_in_r    <= op_in;
-                        a_in_r     <= a_in;
-                        b_in_r     <= b_in;
-                        in_valid_r <= in_valid;
-                end
-        end
-
-        // Compute the result
-        always_comb begin
-                result = '0;
-                if (in_valid_r) begin
-                        case (op_in_r)
-                                add: result = a_in_r + b_in_r;
-                                sub: result = a_in_r + (~b_in_r+1'b1);
-                                default: result = '0;
-                        endcase
-                end
-        end
-
-        // Register outputs
-        always_ff @ (posedge clk, posedge rst) begin
-                if (rst) begin
-                        out       <= '0;
-                        out_valid <= '0;
-                end else begin
-                        out       <= 6'h5;
-                        out_valid <= in_valid_r;
-                end
-        end
-
-endmodule;
+    wire [4:0] out0,out1;
+    wire [3:0] N_B = ~B_i + 4'h1;
+    assign out0 = $signed(A_i) + $signed(B_i);
+    assign out1 = $signed(A_i) + $signed(N_B);
+    wire [3:0] out2 = ~A_i;
+    wire [3:0] out3 = A_i & B_i;
+    wire [3:0] out4 = A_i | B_i;
+    wire [3:0] out5 = A_i ^ B_i;
+    wire [3:0] out6 = (A_i < B_i) ? 4'h1 : 0;
+    wire [3:0] out7 = (A_i == B_i) ? 4'h1 : 0;
+ 
+    always @(*) begin
+      zero_o = 0;
+      overflow_o = 0;
+      carry_o = 0;
+      out_o = 0;
+      case (op_i)
+          3'd0: begin
+            zero_o = ~(|out0[3:0]);
+            overflow_o = (A_i[3] == B_i[3])&&(out0[3] != A_i[3]);
+            carry_o = out0[4];
+            out_o = out0[3:0];
+          end
+          3'd1: begin
+            zero_o = ~(|out1[3:0]);
+            overflow_o = (A_i[3] == N_B[3])&&(out1[3] != A_i[3]);
+            carry_o = out1[4];
+            out_o = out1[3:0];
+          end
+          3'd2: out_o = out2;
+          3'd3: out_o = out3;
+          3'd4: out_o = out4;
+          3'd5: out_o = out5;
+          3'd6: out_o = out6;
+          3'd7: out_o = out7;
+      endcase
+    end
+endmodule //top
