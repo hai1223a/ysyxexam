8a9208eeee02f305bec72b0ab31fa4d53f4ba6a9
tracer-ysyx <tracer@ysyx.org>
2025-04-21 21:46:03 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:46:03 up 12:39,  1 user,  load average: 1.78, 1.20, 0.93

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -97,8 +97,7 @@ static int cmd_x(char *args){
 
 static int cmd_expr(char *args){
   bool success = true;
-  if(success) printf("a");
-  expr(args, &success);
+  if(!args) expr(args, &success);
   // if(!success) assert(0);
   return 0;
 }
