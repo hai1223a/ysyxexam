df29df0f7f32eeb83ed09d2b1675904d239cce11
tracer-ysyx <tracer@ysyx.org>
2025-04-21 21:37:00 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:00 up 12:29,  1 user,  load average: 0.92, 0.75, 0.76

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -76,9 +76,8 @@ static bool make_token(char *e) {
   regmatch_t pmatch;
 
   nr_token = 0;
-  return true;
+
   while (e[position] != '\0') {
-    printf("a");
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
