9d875d832856edd9b6a63db37f1e181ea3cc8fe2
tracer-ysyx <tracer@ysyx.org>
2025-04-21 17:54:17 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:54:17 up  8:47,  1 user,  load average: 0.96, 1.02, 0.87

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -47,6 +47,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
+  printf("%s",p);
   // p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   int i;
@@ -58,12 +59,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #endif
     // p += snprintf(p, 4, " %02x", inst[i]);
   }
-  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-  int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
-  space_len = space_len * 3 + 1;
-  memset(p, ' ', space_len);
-  p += space_len;
+  // int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+  // int space_len = ilen_max - ilen;
+  // if (space_len < 0) space_len = 0;
+  // space_len = space_len * 3 + 1;
+  // memset(p, ' ', space_len);
+  // p += space_len;
 
   // void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   // disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
