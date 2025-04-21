33cf6601efcffc6759852f4cbb21c8f46908261d
tracer-ysyx <tracer@ysyx.org>
2025-04-21 17:55:42 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:55:42 up  8:48,  1 user,  load average: 0.76, 0.96, 0.87

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -49,16 +49,16 @@ static void exec_once(Decode *s, vaddr_t pc) {
   char *p = s->logbuf;
   if(!p) printf("hello");
   // p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  int ilen = s->snpc - s->pc;
-  int i;
-  // uint8_t *inst = (uint8_t *)&s->isa.inst;
-#ifdef CONFIG_ISA_x86
-  for (i = 0; i < ilen; i ++) {
-#else
-  for (i = ilen - 1; i >= 0; i --) {
-#endif
-    // p += snprintf(p, 4, " %02x", inst[i]);
-  }
+//   int ilen = s->snpc - s->pc;
+//   int i;
+//   // uint8_t *inst = (uint8_t *)&s->isa.inst;
+// #ifdef CONFIG_ISA_x86
+//   for (i = 0; i < ilen; i ++) {
+// #else
+//   for (i = ilen - 1; i >= 0; i --) {
+// #endif
+//     // p += snprintf(p, 4, " %02x", inst[i]);
+//   }
   // int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   // int space_len = ilen_max - ilen;
   // if (space_len < 0) space_len = 0;
