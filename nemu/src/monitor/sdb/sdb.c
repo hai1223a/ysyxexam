504269b38d3386e307dc6fad84f235b34d9c9983
tracer-ysyx <tracer@ysyx.org>
2025-04-21 20:17:49 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:17:49 up 11:10,  1 user,  load average: 1.12, 0.92, 0.71

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,7 +89,7 @@ static int cmd_x(char *args){
   printf("Address  Data\n");
   for (int i = 0; i < num_word; i++)
   {
-    printf("%x:%x", base_addr, vaddr_read(base_addr, 4));
+    printf("%x:%x\n", base_addr, vaddr_read(base_addr, 4));
     base_addr += 4;
   }
   return 0;
