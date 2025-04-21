f39af8a46144a303dca29c91e1ed390633550a4c
tracer-ysyx <tracer@ysyx.org>
2025-04-21 17:54:32 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:54:32 up  8:47,  1 user,  load average: 1.05, 1.04, 0.88

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -47,7 +47,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
-  printf("%s",p);
+  printf("aa\n%s",p);
   // p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   int i;
