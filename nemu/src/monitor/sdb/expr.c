63c72ab6fed9f066a70b74ff75495224e8195e08
tracer-ysyx <tracer@ysyx.org>
2025-04-22 09:18:43 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:18:43 up 4 min,  1 user,  load average: 1.53, 0.67, 0.25

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -129,7 +129,7 @@ word_t expr(char *e, bool *success) {
     
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
+  // /* TODO: Insert codes to evaluate the expression. */
   // TODO();
 
   return 0;
