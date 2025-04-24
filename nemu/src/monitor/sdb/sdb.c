4005bf4f8133e532d4e26d4fbfbd43cb14ed47a5
tracer-ysyx <tracer@ysyx.org>
2025-04-24 17:11:00 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:00 up  2:25,  1 user,  load average: 1.35, 1.09, 0.92

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -155,23 +155,23 @@ void sdb_mainloop() {
     return;
   }
 
-  // FILE *fp = fopen("/home/yunhai/ysyx-workbench/nemu/tools/gen-expr/data.txt", "r");
-  // if (fp == NULL) assert(0);
-  // word_t theory_result;
-  // char expression[1001]; // 假设表达式的长度不会超过 255
-  // // 按照 "%u %s\n" 格式读取文件内容，直到文件结束
-  // while (fscanf(fp, "%u %[^\n]", &theory_result, expression) != EOF) {
-  //   printf("expression =%s\n",expression);
-  //   int test_result = cmd_p(expression);  
-  //   printf("test_result: %u, theory_result: %u\n\n", (word_t)test_result, theory_result);
-  //   Assert((word_t)test_result == theory_result, "有问题");
-  // }
-  // fclose(fp);
+  FILE *fp = fopen("/home/yunhai/ysyx-workbench/nemu/tools/gen-expr/data.txt", "r");
+  if (fp == NULL) assert(0);
+  word_t theory_result;
+  char expression[1001]; // 假设表达式的长度不会超过 255
+  // 按照 "%u %s\n" 格式读取文件内容，直到文件结束
+  while (fscanf(fp, "%u %[^\n]", &theory_result, expression) != EOF) {
+    printf("expression =%s\n",expression);
+    int test_result = cmd_p(expression);  
+    printf("test_result: %u, theory_result: %u\n\n", (word_t)test_result, theory_result);
+    Assert((word_t)test_result == theory_result, "有问题");
+  }
+  fclose(fp);
 
-  word_t a = (4-15)/8+1;
-  word_t b = ((unsigned)4-(unsigned)15)/(unsigned)8+(unsigned)1;
+  // word_t a = (4-15)/8+1;
+  // word_t b = ((unsigned)4-(unsigned)15)/(unsigned)8+(unsigned)1;
 
-  printf("a=%d, b=%u\n",a,b);
+  // printf("a=%d, b=%u\n",a,b);
 
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
