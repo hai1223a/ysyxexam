80e99e5848c7ea11d886fbaeb137c3d254350c73
tracer-ysyx <tracer@ysyx.org>
2025-04-24 22:16:58 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:58 up  7:31,  1 user,  load average: 1.18, 0.99, 1.01

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -24,7 +24,7 @@ static const uint32_t img [] = {
   // 0x0102c503,  // lbu a0,16(t0)
   // 0x00100073,  // ebreak (used as nemu_trap)
   // 0xdeadbeef,  // some data
-  0x00100093,  // li t0, 1       (addi t0, zero, 1) 将 t0 赋值为 1
+  0x00000097,  // li t0, 1       (addi t0, zero, 1) 将 t0 赋值为 1
   0x00000013,  // nop            (addi zero, zero, 0) 第 1 个 nop
   0x00000013,  // nop            (addi zero, zero, 0) 第 2 个 nop
   0x00000013,  // nop            (addi zero, zero, 0) 第 3 个 nop
