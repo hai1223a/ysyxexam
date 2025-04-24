090d42159b02f4062e7bbef313573cabd225e224
tracer-ysyx <tracer@ysyx.org>
2025-04-24 16:01:39 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:01:39 up  1:16,  1 user,  load average: 2.11, 1.40, 1.20

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -168,9 +168,8 @@ void sdb_mainloop() {
   // }
   // fclose(fp);
 
-  word_t c = 0;
   word_t a = (( ( 8/5))- ((1)-(4-(3*5))/8)+ 3-9/5/1- (((1))));
-  word_t b = c+(c+2-3);
+  word_t b = ((word_t)2-(word_t)3)+(word_t)1;
 
   printf("a=%u, b=%u\n",a,b);
 
