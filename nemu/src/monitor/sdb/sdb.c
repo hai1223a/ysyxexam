5f259846974ec7abb4e5f3b807d911ed15b8e4af
tracer-ysyx <tracer@ysyx.org>
2025-04-24 15:20:23 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:23 up 34 min,  1 user,  load average: 2.02, 1.52, 1.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -161,8 +161,8 @@ void sdb_mainloop() {
   char expression[65536]; // 假设表达式的长度不会超过 255
 
   // 按照 "%u %s\n" 格式读取文件内容，直到文件结束
-  while (fscanf(fp, "%u %s\n", &theory_result, expression) != EOF) {
-    printf("expression=%s\n",expression);
+  while (fscanf(fp, "%u %[^\n]", &theory_result, expression) != EOF) {
+    printf("expression =%s\n",expression);
     int test_result = cmd_p(expression);  
     printf("test_result: %u, theory_result: %u\n\n", (word_t)test_result, theory_result);
   }
