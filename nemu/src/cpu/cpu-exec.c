3f7fe77c534834d04fa5f94acaae22abff1c62ba
tracer-ysyx <tracer@ysyx.org>
2025-04-25 11:44:15 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:44:15 up  1:29,  1 user,  load average: 1.11, 0.99, 1.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,9 +42,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   word_t data_pre[NR_WP] = {0};
   word_t data_new[NR_WP] = {0};
   int index[NR_WP] = {0};
-  
   scan_watchpoint(data_new, index);
-  printf("data_pre == %u, data_new == %u", data_pre[0], data_new[0]);
+  printf("data_pre == %u, data_new == %u, index == %d", data_pre[0], data_new[0], index[0]);
   for (int i = 0; i < NR_WP; i++)
   {
     if(index[i])
