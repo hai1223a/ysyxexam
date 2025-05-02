259461c918e7aeb20c56238bab33b1ed5affe28c
tracer-ysyx <tracer@ysyx.org>
2025-05-02 22:27:37 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:27:37 up  7:22,  1 user,  load average: 0.65, 0.81, 0.66

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -61,10 +61,9 @@ static int decode_exec(Decode *s) {
   int rd = 0; \
   word_t src1 = 0, src2 = 0, imm = 0; \
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
-  printf("imm = %d  %u  %x \n",(int)imm, imm, imm); \
   __VA_ARGS__ ; \
 }
-
+  // printf("imm = %d  %u  %x \n",(int)imm, imm, imm); 
   INSTPAT_START();
   // RV32I
   /*
