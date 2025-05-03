bdeac38d980c20720c649d127a4dbb3709470daa
tracer-ysyx <tracer@ysyx.org>
2025-05-03 19:11:16 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:11:16 up  1:19,  1 user,  load average: 1.07, 1.03, 1.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -38,7 +38,7 @@ enum {
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | \
                     (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1) | 0; } while(0)
-#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BIT(i, 7, 7) << 11) | \
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | \
                     (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1) | 0;} while (0)
 
 
@@ -68,6 +68,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_R: src1R(); src2R();         break;    
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
