4bb337ae963619604b8558060f90757f745ab984
tracer-ysyx <tracer@ysyx.org>
2025-05-04 20:05:02 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:05:02 up  5:52,  1 user,  load average: 1.07, 0.85, 0.40

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -49,7 +49,7 @@ enum {
   // 移位 
   SRA, SLL, SRL, 
   // 乘除法
-  MUL, DIV, 
+  MUL, DIV, REM,
 };
 
 #define src1R() do { *src1 = Reg(rs1); } while (0)
@@ -89,10 +89,11 @@ static word_t mul_div(const word_t op1, const word_t op2, int op) {
   switch (op)
   {
   case MUL:
-    return op1 * op2;
-    break;
+    return (int32_t)op1 * (int32_t)op2;
   case DIV:
     return (int32_t)op1 / (int32_t)op2;
+  case REM:
+    return (int32_t)op1 % (int32_t)op2;
   default:
     return 0;
   }
@@ -162,6 +163,7 @@ static int decode_exec(Decode *s) {
   //RV32M
   INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul_   , R, Reg(rd) = mul_div(src1, src2, MUL));
   INSTPAT("0000001 ????? ????? 100 ????? 01100 11", div_   , R, Reg(rd) = mul_div(src1, src2, DIV));
+  INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem_   , R, Reg(rd) = mul_div(src1, src2, REM));
 
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
