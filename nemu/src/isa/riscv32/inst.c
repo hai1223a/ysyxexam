4183c3425e3dcc7addb0f7d66e5f2936bbca4817
tracer-ysyx <tracer@ysyx.org>
2025-05-04 21:29:00 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:59 up  7:16,  1 user,  load average: 1.53, 1.68, 1.31

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -87,15 +87,17 @@ static word_t alu(const word_t op1, const word_t op2, int op) {
 
 static word_t mul_div(const word_t op1, const word_t op2, int op) {
   int64_t result_mul;
+  int32_t int_op1 = (int32_t)op1;
+  int32_t int_op2 = (int32_t)op2;
   switch (op)
   {
-    case MUL:     return (int32_t)op1 * (int32_t)op2;
+    case MUL:     return int_op1 * int_op2;
     case MULH:
-      result_mul = (int32_t)op1 * (int32_t)op2;
+      result_mul = (int64_t)int_op1 * (int64_t)int_op2;
       return (word_t)BITS(result_mul, 63, 32);
-    case DIV:     return (int32_t)op1 / (int32_t)op2;
+    case DIV:     return int_op1 / int_op2;
     case DIVU:    return op1 / op2;
-    case REM:     return (int32_t)op1 % (int32_t)op2;
+    case REM:     return int_op1 % int_op2;
     case REMU:    return op1 % op2;
     default:      return 0;
   }
