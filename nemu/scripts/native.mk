a8197d314b0ac78535f800d192b17337422994bc
tracer-ysyx <tracer@ysyx.org>
2025-05-04 22:26:12 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:12 up  8:13,  1 user,  load average: 0.50, 0.68, 0.81

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,7 +24,7 @@ $(BINARY):: compile_git
 
 # Some convenient rules
 
-ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
+ARGS += --log=$(BUILD_DIR)/nemu-log.txt
 ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
