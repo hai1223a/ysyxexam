34cfdb36a07894aaaf53b169aa5bc9f900f84b2a
tracer-ysyx <tracer@ysyx.org>
2025-05-05 15:20:21 +0800

    > compile NEMU ysyx_22040000 谢平江 Linux yunhai 6.11.0-24-generic #24~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 25 20:14:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:21 up 1 day,  1:07,  1 user,  load average: 0.68, 0.67, 0.46

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -138,7 +138,7 @@ static int decode_exec(Decode *s) {
   word_t a = 2147483648;
   word_t b = 4294967295;
   int32_t c = (int32_t)a / (int32_t)b;
-  printf("a/b = %d, a/b = %d", (int32_t)a / (int32_t)b, c);
+  printf("a/b = %d, a/b = %x", (int32_t)a / (int32_t)b, c);
   // printf("a*b = %x, a*b = %x, a*b = %lx, a*b = %llx\n", (word_t)BITS((int64_t)a * (int64_t)a, 63, 32), (int32_t)a*(int32_t)b, (int64_t)a*(int64_t)b, (long long)a*(long long)b);
 #define INSTPAT_INST(s) ((s)->isa.inst)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
