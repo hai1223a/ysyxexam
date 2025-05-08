6dacd746bf9e6883202a268067546e76e4116dc9
tracer-ysyx <tracer@ysyx.org>
2025-05-08 20:54:53 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:53 up 10:05,  1 user,  load average: 1.27, 0.96, 0.86

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -76,6 +76,9 @@ static void monitor_check(Decode *_this) {
   } IRINGBUF = {0};
 
   static void print_iringbuf() {
+    printf("iringbuf打印\n");
+    printf("如果是发生了 HIT BAD TRAP 或者 ABORT, 则--->指向发生问题的指令\n");
+    printf("如果是发生了 assert 0 报错的话, 则--->指向发生问题的指令的上一条指令\n");
     for(int i = 0; i < IRINGBUF_DEEPTH; i++) {
       if(i == IRINGBUF.now_p) 
         printf("--->");
