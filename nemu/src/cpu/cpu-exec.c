1e01aaa70ebe8ad25c52441a78beb9204fe73a9a
tracer-ysyx <tracer@ysyx.org>
2025-05-08 13:52:24 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:52:24 up  3:02,  1 user,  load average: 0.58, 0.37, 0.37

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -117,7 +117,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
   // 这里也是IRINGBUF部分的代码
   //===============================================
-  strcpy(IRINGBUF.iringbuf[IRINGBUF.p], s->logbuf);
+  strncpy(IRINGBUF.iringbuf[IRINGBUF.p], s->logbuf, 128);
   if(IRINGBUF.p < IRINGBUF_DEEPTH)
     IRINGBUF.p++;
   else
