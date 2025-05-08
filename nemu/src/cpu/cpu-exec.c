6a2f9f4015e61380b7deb11920df45f7433b7584
tracer-ysyx <tracer@ysyx.org>
2025-05-08 19:34:24 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:34:24 up  8:44,  1 user,  load average: 1.14, 0.97, 0.81

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -181,7 +181,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-      IFDEF(CONFIG_ITRACE,if(nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0)
+      IFDEF(CONFIG_ITRACE,if(nemu_state.state != NEMU_ABORT || nemu_state.halt_ret != 0)
       print_iringbuf());
 
       // fall through
