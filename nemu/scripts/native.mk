2e2d1261227246af22d23a8871a138157195fbe9
tracer-ysyx <tracer@ysyx.org>
2025-05-09 16:53:41 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:41 up 1 day,  6:04,  1 user,  load average: 0.52, 0.46, 0.24

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -29,7 +29,8 @@ override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?=
-NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
+ELF_FILE ?=
+NEMU_EXEC := $(BINARY) $(ARGS) $(IMG) --elf=$(ELF_FILE)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
