e85e667fe24b9d0489c14f13f34f602dfd6d8b9d
tracer-ysyx <tracer@ysyx.org>
2025-05-11 11:36:44 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:36:44 up 45 min,  1 user,  load average: 0.89, 1.06, 1.01

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -37,6 +37,7 @@ bool log_enable() {
 
 // ftracer 部分内容
 //=========================================
+#ifdef CONFIG_FTRACER
 FILE *ftracer_log_fp = NULL;
 void init_ftracer_log(const char *ftracer_log_file) {
   ftracer_log_fp = stdout;
@@ -48,4 +49,5 @@ void init_ftracer_log(const char *ftracer_log_file) {
   Log("Ftracer log is written to %s", ftracer_log_file ? ftracer_log_file : "stdout");
 }
 //=========================================
+#endif
 #endif
