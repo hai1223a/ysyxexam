3185ebf67bb46ae37173a00b13b9ea089487b358
tracer-ysyx <tracer@ysyx.org>
2025-05-21 20:39:55 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:39:55 up  7:28,  1 user,  load average: 0.92, 0.95, 0.82

---
+++ npc/obj_dir/Vysyx_25050136_NPC___024root.h
@@ -0,0 +1,52 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vysyx_25050136_NPC.h for the primary calling header
+
+#ifndef VERILATED_VYSYX_25050136_NPC___024ROOT_H_
+#define VERILATED_VYSYX_25050136_NPC___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+
+class Vysyx_25050136_NPC__Syms;
+
+class alignas(VL_CACHE_LINE_BYTES) Vysyx_25050136_NPC___024root final : public VerilatedModule {
+  public:
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clk,0,0);
+    VL_IN8(reset,0,0);
+    VL_OUT8(mem_ren_o,0,0);
+    VL_OUT8(mem_wen_o,0,0);
+    VL_OUT8(mem_len_o,2,0);
+    CData/*0:0*/ __VstlFirstIteration;
+    CData/*0:0*/ __VicoFirstIteration;
+    CData/*0:0*/ __Vtrigprevexpr___TOP__clk__0;
+    CData/*0:0*/ __VactContinue;
+    VL_IN(inst_i,31,0);
+    VL_IN(mem_rdata_i,31,0);
+    VL_OUT(mem_wdata_o,31,0);
+    VL_OUT(mem_addr_o,31,0);
+    VL_OUT(pc_o,31,0);
+    IData/*31:0*/ ysyx_25050136_NPC__DOT__u_ysyx_25050136_IF__DOT__pc;
+    IData/*31:0*/ ysyx_25050136_NPC__DOT__u_ysyx_25050136_IF__DOT__static_npc;
+    IData/*31:0*/ __VactIterCount;
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<1> __VicoTriggered;
+    VlTriggerVec<1> __VactTriggered;
+    VlTriggerVec<1> __VnbaTriggered;
+
+    // INTERNAL VARIABLES
+    Vysyx_25050136_NPC__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    Vysyx_25050136_NPC___024root(Vysyx_25050136_NPC__Syms* symsp, const char* v__name);
+    ~Vysyx_25050136_NPC___024root();
+    VL_UNCOPYABLE(Vysyx_25050136_NPC___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+};
+
+
+#endif  // guard
