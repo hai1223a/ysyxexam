f2838ca81258cdfdc603818e794553c71ec5bd0b
tracer-ysyx <tracer@ysyx.org>
2025-05-22 23:46:29 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:46:29 up  8:30,  1 user,  load average: 1.37, 1.09, 0.97

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,7 +3,7 @@
 #include <getopt.h>              // 包含解析命令行参数的库函数
 #include <verilated.h>           // Verilator的库
 #include "verilated_fst_c.h"     // fst波形文件所需要的库
-#include "sdb.h"
+#include "../incldue/sdb.h"
 //=====================================================
 // 全局变量和宏定义
 //=====================================================
