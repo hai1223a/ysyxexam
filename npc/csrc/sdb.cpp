be5a1310c2fbb0ca7fff854a60979414525a5a3f
tracer-ysyx <tracer@ysyx.org>
2025-05-23 00:14:47 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-25-generic #25~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:20:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:14:47 up  8:58,  1 user,  load average: 1.20, 1.36, 1.14

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -139,7 +139,7 @@ void sdb_mainloop(Vysyx_25050136_NPC *ysyx_25050136_NPC) {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) return; 
+        if (cmd_table[i].handler(args, ysyx_25050136_NPC) < 0) return; 
         break;
       }
     }
