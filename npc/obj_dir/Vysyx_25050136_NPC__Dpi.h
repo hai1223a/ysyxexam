b01cdb08bfb64a1e14b0e3b024a26c7cc0a22e1a
tracer-ysyx <tracer@ysyx.org>
2025-05-23 17:37:41 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:37:41 up  4:06,  1 user,  load average: 0.75, 1.03, 0.98

---
+++ npc/obj_dir/Vysyx_25050136_NPC__Dpi.h
@@ -0,0 +1,26 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VYSYX_25050136_NPC__DPI_H_
+#define VERILATED_VYSYX_25050136_NPC__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at vsrc/top.v:2:30
+    extern void find_ebreak(svBit find);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
