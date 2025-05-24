0be9fea218a75f092dd4510c7171fe45949b8073
tracer-ysyx <tracer@ysyx.org>
2025-05-24 23:00:15 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:00:15 up  5:00,  1 user,  load average: 1.15, 1.23, 1.20

--- npc/csrc/expr.cpp
+++ npc/csrc/expr.cpp
@@ -15,7 +15,7 @@
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
-#include <regex.h>
+
 #include "../include/common.h"
 
 enum {
