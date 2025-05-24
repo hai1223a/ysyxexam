ddafb4c3872e775211390b976166e057e6198b0d
tracer-ysyx <tracer@ysyx.org>
2025-05-24 23:26:02 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:26:02 up  5:26,  1 user,  load average: 1.60, 1.19, 1.21

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -54,6 +54,7 @@ void printf_statu(Vysyx_25050136_NPC *ysyx_25050136_NPC)
 //=====================================================
 int main(int argc, char **argv)
 {
+  expr("1+1", NULL);
   // 解析命令行参数
   parse_args(argc, argv);
   // 传递参数给verilator,建议在创建任何模型之前使用
