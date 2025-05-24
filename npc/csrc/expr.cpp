9a76242d06297bbd77ffec8de89ca5f084a2afc1
tracer-ysyx <tracer@ysyx.org>
2025-05-24 23:13:31 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:13:31 up  5:14,  1 user,  load average: 2.63, 1.53, 1.32

--- npc/csrc/expr.cpp
+++ npc/csrc/expr.cpp
@@ -154,7 +154,7 @@ static bool make_token(char *e, int *valid_tokens) {
         break;
       }
     }
-
+    AA
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
