726675cd8ae839d2b8d116555f81d12cb95fbb41
tracer-ysyx <tracer@ysyx.org>
2025-05-24 22:38:11 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:38:11 up  4:38,  1 user,  load average: 0.98, 1.06, 1.04

--- npc/csrc/expr.cpp
+++ npc/csrc/expr.cpp
@@ -346,6 +346,7 @@ uint32_t expr(Vysyx_25050136_NPC *ysyx_25050136_NPC, char *e, bool *success) {
     *success = false;
     return 0;
   }
+  printf("a");
   *success = true;
   uint32_t result = eval(ysyx_25050136_NPC, 0, valid_tokens-1);
   return result;
