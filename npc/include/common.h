251518d4a420c18bffc47842402753e7798d6cc2
tracer-ysyx <tracer@ysyx.org>
2025-05-24 21:47:24 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:24 up  3:48,  1 user,  load average: 2.49, 1.81, 1.38

--- npc/include/common.h
+++ npc/include/common.h
@@ -15,20 +15,17 @@
 //=====================================================
 // 全局变量和宏定义
 //=====================================================
-bool cpu_run = true;                          // CPU仿真运行状态
-vluint64_t sim_time = 0;                      // 记录仿真时间
-vluint64_t reset_time = 10;                   // 复位时间
-vluint64_t stop_time = 0;                     // 暂停时间点
-uint32_t pc_pre = 0x80000000;                 // 用于单步执行程序
-static char *img_file = NULL;                 // 程序源文件指针
-bool batch_mode = false;                        // sdb模式
 #define CONFIG_MSIZE 0x8000000                // 内存大小
 #define CONFIG_MBASE 0x80000000               // 内存基地址
 
-static uint8_t pmem[CONFIG_MSIZE] __attribute((aligned(4096))) = {};// 内存变量
-const char *regs[] = {
-    "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5"
-  };
+extern bool cpu_run;                                           // CPU仿真运行状态
+extern vluint64_t sim_time;                                    // 记录仿真时间
+extern vluint64_t reset_time;                                  // 复位时间
+extern vluint64_t stop_time;                                   // 暂停时间点
+extern uint32_t pc_pre;                                        // 用于单步执行程序
+extern char *img_file;                                         // 程序源文件指针
+extern bool batch_mode;                                        // sdb模式
+extern uint8_t pmem[CONFIG_MSIZE] __attribute((aligned(4096)));// 内存变量
+extern const char *regs[];
 
 #endif
