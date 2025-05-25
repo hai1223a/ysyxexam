a960b200802a29caccae075a1fa23eeae7276461
tracer-ysyx <tracer@ysyx.org>
2025-05-25 18:48:56 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:48:56 up 12 min,  1 user,  load average: 0.75, 0.98, 0.62

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -23,7 +23,7 @@ static const uint32_t img [] = {
   0x00028823,  // sb  zero,16(t0)
   0x0102c503,  // lbu a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
-  0xdeadbeef,  // some data
+  0xdeadbeef  // some data
 };
 
 static void restart() {
--- npc/include/expr.h
+++
@@ -1,7 +0,0 @@
-#ifndef __EXPR_H__
-#define __EXPR_H__
-
-void init_regex();
-uint32_t expr(Vysyx_25050136_NPC *ysyx_25050136_NPC, char *e, bool *success);
-
-#endif
