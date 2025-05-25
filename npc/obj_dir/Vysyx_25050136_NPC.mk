f29d22076dd780e8fe8383646c7e491c8c168848
tracer-ysyx <tracer@ysyx.org>
2025-05-25 15:48:18 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:48:18 up  2:54,  1 user,  load average: 1.52, 1.28, 1.08

--- npc/obj_dir/Vysyx_25050136_NPC.mk
+++ npc/obj_dir/Vysyx_25050136_NPC.mk
@@ -53,7 +53,6 @@ VM_USER_CLASSES = \
 	memory \
 	reg \
 	sdb \
-	tracer \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
@@ -85,8 +84,6 @@ memory.o: csrc/memory.cpp
 reg.o: csrc/reg.cpp 
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
 sdb.o: csrc/sdb.cpp 
-	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
-tracer.o: csrc/tracer.cpp 
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
 
 ### Link rules... (from --exe)
