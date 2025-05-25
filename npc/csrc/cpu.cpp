d2147fba45be27e719eb7d91022f87bf2ece8cb4
tracer-ysyx <tracer@ysyx.org>
2025-05-25 16:09:47 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:47 up  3:15,  1 user,  load average: 1.94, 1.24, 1.04

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -48,7 +48,7 @@ void trace_and_difftest(Vysyx_25050136_NPC *ysyx_25050136_NPC)
   for (i = ilen - 1; i >= 0; i --) {
     printf(" %02x", inst[i]);
   }
-  disassemble(p, logbuf + sizeof(logbuf) - p, pc, (uint8_t *)&inst_i, ilen);
+  // disassemble(p, logbuf + sizeof(logbuf) - p, pc, (uint8_t *)&inst_i, ilen);
   // 这里也是IRINGBUF部分的代码
   //===============================================
   IRINGBUF.now_p = IRINGBUF.p;
