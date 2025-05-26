1c28bb3ce2d4cceaf1c8ed4f7e9a2012617c05ae
tracer-ysyx <tracer@ysyx.org>
2025-05-26 18:45:49 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:45:49 up 6 min,  1 user,  load average: 1.40, 1.07, 0.50

--- npc/include/macro.h
+++ npc/include/macro.h
@@ -1,21 +1,7 @@
 #ifndef __MACRO_H__
 #define __MACRO_H__
-//=====================================================
-// 通用宏定义
-//=====================================================
-#define ANSI_FG_RED     "\33[1;31m"           // 终端红色输出
-#define ANSI_FG_GREEN   "\33[1;32m"           // 终端绿色输出
-#define ANSI_NONE       "\33[0m"              
-#define ANSI_FMT(str, fmt) fmt str ANSI_NONE  // 用于输出有颜色的终端信息
 
-// Assert
-#define Assert(cond, format, ...) \
-do { \
-    if (!(cond)) { \
-      printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__); \
-      assert(cond); \
-    } \
-  } while (0)
+#include <string.h>
 
 // macro stringizing
 #define str_temp(x) #x
@@ -95,5 +81,20 @@ do { \
 #define likely(cond)   __builtin_expect(cond, 1)
 #define unlikely(cond) __builtin_expect(cond, 0)
 #endif
+
+// 终端颜色输出
+#define ANSI_FG_RED     "\33[1;31m"           // 终端红色输出
+#define ANSI_FG_GREEN   "\33[1;32m"           // 终端绿色输出
+#define ANSI_NONE       "\33[0m"              
+#define ANSI_FMT(str, fmt) fmt str ANSI_NONE  // 用于输出有颜色的终端信息
+
+// Assert函数
+#define Assert(cond, format, ...) \
+do { \
+    if (!(cond)) { \
+      printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__); \
+      assert(cond); \
+    } \
+  } while (0)
 
 #endif
