3f24f84207fb503c85d4cd89e90c21126c31e013
tracer-ysyx <tracer@ysyx.org>
2025-05-26 19:12:26 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:12:26 up 33 min,  1 user,  load average: 0.86, 1.21, 1.15

--- npc/csrc/disasm.cpp
+++ npc/csrc/disasm.cpp
@@ -39,7 +39,7 @@ void init_disasm() {
   assert(cs_free_dl);
 
   cs_arch arch = CS_ARCH_RISCV;
-  cs_mode mode = CS_MODE_RISCV32 | CS_MODE_RISCVC;
+  cs_mode mode = CS_MODE_RISCV32;
 	int ret = cs_open_dl(arch, mode, &handle);
   assert(ret == CS_ERR_OK);
 
