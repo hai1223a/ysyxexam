6237895f7f0b5ccd6ec8254858943d8505b97f9e
tracer-ysyx <tracer@ysyx.org>
2025-05-26 23:51:38 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:51:38 up  5:12,  1 user,  load average: 2.01, 1.63, 1.30

--- npc/include/common.h
+++ npc/include/common.h
@@ -24,7 +24,7 @@
 #define NR_WP 5
 
 #define CONFIG_ITRACE
-// #define CONFIG_MTRACE
+#define CONFIG_MTRACE
 
 extern bool cpu_run;                                           // CPU仿真运行状态
 extern vluint64_t sim_time;                                    // 记录仿真时间
