72ac413b92aa61c85abf080e6f2508922a4eee88
tracer-ysyx <tracer@ysyx.org>
2025-05-26 19:41:17 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:41:17 up  1:02,  1 user,  load average: 1.16, 1.09, 1.05

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -88,7 +88,7 @@ void cpu_exec_once(Vysyx_25050136_NPC *ysyx_25050136_NPC, VerilatedFstC *tfp)
     // 模拟时钟反转
     ysyx_25050136_NPC->clk ^= 1;
     // 计算电路状态
-    ysyx_25050136_NPC->eval();
+    // ysyx_25050136_NPC->eval();
     // 复位
     reset(ysyx_25050136_NPC, sim_time);
     // 取指
@@ -97,7 +97,7 @@ void cpu_exec_once(Vysyx_25050136_NPC *ysyx_25050136_NPC, VerilatedFstC *tfp)
     pmem_read_write(ysyx_25050136_NPC);
     // 计算电路状态
     if(ysyx_25050136_NPC->inst_i == 0x00100073) cpu_run = false;
-    // ysyx_25050136_NPC->eval();
+    ysyx_25050136_NPC->eval();
     // 记录波形数据
     tfp->dump(sim_time);
     // 推动仿真进行
