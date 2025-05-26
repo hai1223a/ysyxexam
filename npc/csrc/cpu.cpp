5570bcc682ad2e1a9272825a76d15671335623c7
tracer-ysyx <tracer@ysyx.org>
2025-05-26 23:53:01 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:53:01 up  5:13,  1 user,  load average: 2.24, 1.77, 1.38

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -60,7 +60,7 @@ void cpu_exec(Vysyx_25050136_NPC *ysyx_25050136_NPC, VerilatedFstC *tfp, uint32_
     cpu_exec_once(ysyx_25050136_NPC, tfp);
 #ifdef CONFIG_ITRACE
     Itrace(inst__, pc__);
-    if(inst_num < PRINT_INST_NUM)
+    // if(inst_num < PRINT_INST_NUM)
       printf("%s\n", itrace_buf);
 #endif
     if(likely(!batch_mode)) {
