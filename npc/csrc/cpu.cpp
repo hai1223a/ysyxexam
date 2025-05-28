2ba70cbd23ff02cd96b3773902fa49a66a0407d4
tracer-ysyx <tracer@ysyx.org>
2025-05-28 22:03:50 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:50 up  7:29,  1 user,  load average: 1.08, 1.09, 1.03

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -7,7 +7,7 @@ void reset(Vysyx_25050136_NPC *ysyx_25050136_NPC, vluint64_t &sim_time)
     ysyx_25050136_NPC->reset = 1;
 }
 
-void cpu_init() {
+void cpu_init(Vysyx_25050136_NPC *ysyx_25050136_NPC, VerilatedFstC *tfp) {
     stop_time = sim_time;
     long size = pmem_init();
     pc_pre = 0x80000000;
@@ -92,7 +92,7 @@ void cpu_exec(Vysyx_25050136_NPC *ysyx_25050136_NPC, VerilatedFstC *tfp, uint32_
 
 int batch_mainloop(Vysyx_25050136_NPC *ysyx_25050136_NPC, VerilatedFstC *tfp)
 {
-  cpu_init();
+  cpu_init(ysyx_25050136_NPC, tfp);
   cpu_exec(ysyx_25050136_NPC, tfp, -1);
   return 0;
 }
