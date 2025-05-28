2149d251ee1ca7e850bc28425738e9fef02480a7
tracer-ysyx <tracer@ysyx.org>
2025-05-28 21:09:35 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:35 up  6:35,  1 user,  load average: 0.54, 0.24, 0.13

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -23,14 +23,14 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
   {
     for (size_t i = 0; i < n; i++)
     {
-      *(uint8_t *)buf = paddr_read(addr + i, 1);
+      *((uint8_t *)buf + i) = paddr_read(addr + i, 1);
     }
   }
   if (direction == DIFFTEST_TO_REF)
   {
     for (size_t i = 0; i < n; i++)
     {
-      paddr_write(addr + i, 1, (word_t)*(uint8_t *)buf);
+      paddr_write(addr + i, 1, (word_t)*((uint8_t *)buf + i));
     }
   }
 }
