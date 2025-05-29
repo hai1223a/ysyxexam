bcdce3a597c48e8f8d8288e76082865b50388f4d
tracer-ysyx <tracer@ysyx.org>
2025-05-29 15:13:53 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:53 up 1 day, 39 min,  1 user,  load average: 0.85, 0.98, 1.02

--- npc/csrc/reg.cpp
+++ npc/csrc/reg.cpp
@@ -2,11 +2,12 @@
 
 void printf_regs()
 {
+  printf(ANSI_FMT("npc的寄存器状态如下:\n", ANSI_FG_MAGENTA));
   for (size_t i = 0; i < REG_NUM; i++)
   {
     printf("%s : 0x%8x\n", regs[i], ysyx_25050136_NPC->rootp->ysyx_25050136_NPC__DOT__u_ysyx_25050136_RegisterFile__DOT__gpr[i]);
   }
-  printf("pc = 0x%8x\n", ysyx_25050136_NPC->pc_o);
+  printf("pc : 0x%8x\n", ysyx_25050136_NPC->pc_o);
 }
 
 uint32_t reg_str2val(const char *s, bool *success)
