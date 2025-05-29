6458392b16640103a6a1fdcfadeb3e0f4308da2a
tracer-ysyx <tracer@ysyx.org>
2025-05-29 15:32:42 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:42 up 1 day, 58 min,  1 user,  load average: 0.81, 1.11, 1.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -7,10 +7,8 @@ int main(int argc, char **argv)
   init_main(argc, argv);
   if(batch_mode)
     batch_mainloop();
-  else{
-    init_sdb();
+  else
     sdb_mainloop();
-  }  
   npc_end();
   // 结束
   return 0;
