0d49a6ec1a8367c4433dd0cfd881693b156ad0a7
tracer-ysyx <tracer@ysyx.org>
2025-05-29 19:26:10 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:10 up 1 day,  4:52,  1 user,  load average: 1.17, 0.99, 0.48

--- npc/csrc/debug.cpp
+++ npc/csrc/debug.cpp
@@ -92,7 +92,7 @@ void cpu_init()
 {
   npcstate.state = NPC_RUNNING;
   stop_time = sim_time;
-  pc_pre = RESET_VECTOR;
+  pc__ = RESET_VECTOR;
   ysyx_25050136_NPC->clk = 0;
   ysyx_25050136_NPC->inst_i = 0;
   ysyx_25050136_NPC->mem_rdata_i = 0;
--- npc/include/cpu.h
+++ npc/include/cpu.h
@@ -1,8 +1,9 @@
 #ifndef __CPU_H__
 #define __CPU_H__
 
-extern vluint64_t sim_time;      // 记录仿真时间
-extern vluint64_t stop_time;     // 暂停时间点
+extern uint32_t pc__;        // 执行完指令后的PC值
+extern vluint64_t sim_time;  // 记录仿真时间
+extern vluint64_t stop_time; // 暂停时间点
 
 void reset();
 void cpu_exec(uint32_t inst_num);
