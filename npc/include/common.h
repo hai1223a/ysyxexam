64cc51e682d3cc9500215f63b7653437a2dbfdac
tracer-ysyx <tracer@ysyx.org>
2025-05-31 15:56:07 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:56:07 up  1:38,  1 user,  load average: 0.75, 0.77, 0.79

--- npc/include/common.h
+++ npc/include/common.h
@@ -22,7 +22,7 @@
 // 配置选项
 //=====================================================
 #define CONFIG_ITRACE 1
-#define CONFIG_MTRACE 1
+// #define CONFIG_MTRACE 1
 #define CONFIG_FTRACE 1
 #define CONFIG_DIFFTEST 1
 //=====================================================
