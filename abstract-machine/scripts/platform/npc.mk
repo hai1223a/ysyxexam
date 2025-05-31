51234eac77189e1c803c4f0ab77ba6776cd56d06
tracer-ysyx <tracer@ysyx.org>
2025-06-01 01:19:38 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:19:38 up 11:02,  1 user,  load average: 1.24, 1.07, 1.25

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -19,7 +19,7 @@ override NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-$(IMAGE_NAME)-log.txt 
                      -p 1234 \
                      -e $(IMAGE).elf \
                      -g $(shell dirname $(IMAGE).elf)/npc-$(IMAGE_NAME)-ftracerlog.txt \
-                     
+                     -b
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
