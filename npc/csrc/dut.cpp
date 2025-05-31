8f49e40e3e0983343b963449751414ba72f12d5f
tracer-ysyx <tracer@ysyx.org>
2025-05-31 20:28:23 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:28:23 up  6:11,  1 user,  load average: 1.29, 1.45, 1.27

--- npc/csrc/dut.cpp
+++ npc/csrc/dut.cpp
@@ -91,7 +91,5 @@ void difftest_step()
   CPU_state ref_r;
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-  printf_ref_regs(&ref_r);
-  printf_regs();
   checkregs(&ref_r);
 }
