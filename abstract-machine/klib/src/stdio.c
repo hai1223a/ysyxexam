eda1dfbe1062121843c160428435f8f216e2b108
tracer-ysyx <tracer@ysyx.org>
2025-06-04 19:55:01 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:55:01 up  3:16,  1 user,  load average: 0.60, 0.67, 0.81

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,45 +10,125 @@ int printf(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   int count = 0;
-  while (*fmt)
-  {
-    if(*fmt == '%') {
+  while (*fmt) {
+    if (*fmt == '%') {
       fmt++;
-      switch (*fmt)
-      {
-        case 'd':
+      // 处理零填充和宽度
+      int zero_pad = 0, width = 0;
+      if (*fmt == '0') {
+        zero_pad = 1;
+        fmt++;
+      }
+      while (*fmt >= '0' && *fmt <= '9') {
+        width = width * 10 + (*fmt - '0');
+        fmt++;
+      }
+      switch (*fmt) {
+        case 'd': {
           int num = va_arg(ap, int);
-          char num_str[10];
-          int n = 0;
-          if(num < 0) {
-            putch('-');
-            count++;
-            if (num == INT_MIN) {
-              // 特殊处理 INT_MIN
-              num = -(num + 1); // 避免溢出
-              num_str[n++] = (num % 10) + 1 + '0'; // 处理最后一位
-              num /= 10;
-            } else {
-              num = -num;
-            }
+          char num_str[16];
+          int n = 0, neg = 0;
+          unsigned int unum;
+          if (num < 0) {
+            neg = 1;
+            unum = (unsigned int)(-num);
+          } else {
+            unum = (unsigned int)num;
           }
           do {
-            num_str[n] = (num % 10) + '0';
-            n++;
-            num = num / 10;
-          } while(num > 0);
-          while(n) {
-            n--;
-            putch(num_str[n]);
+            num_str[n++] = (unum % 10) + '0';
+            unum /= 10;
+          } while (unum > 0);
+          if (neg) num_str[n++] = '-';
+          int pad = width - n;
+          if (zero_pad && pad > 0) {
+            if (neg) {
+              putch('-');
+              count++;
+              n--;
+            }
+            for (int i = 0; i < pad; i++) {
+              putch('0');
+              count++;
+            }
+            for (int i = n - 1; i >= (neg ? 1 : 0); i--) {
+              putch(num_str[i]);
+              count++;
+            }
+          } else {
+            for (int i = 0; i < pad; i++) {
+              putch(' ');
+              count++;
+            }
+            for (int i = n - 1; i >= 0; i--) {
+              putch(num_str[i]);
+              count++;
+            }
+          }
+          break;
+        }
+        case 'u': {
+          unsigned int num = va_arg(ap, unsigned int);
+          char num_str[16];
+          int n = 0;
+          do {
+            num_str[n++] = (num % 10) + '0';
+            num /= 10;
+          } while (num > 0);
+          int pad = width - n;
+          for (int i = 0; i < pad; i++) {
+            putch(zero_pad ? '0' : ' ');
+            count++;
+          }
+          for (int i = n - 1; i >= 0; i--) {
+            putch(num_str[i]);
             count++;
           }
           break;
-        case 's':
+        }
+        case 'x':
+        case 'X': {
+          unsigned int num = va_arg(ap, unsigned int);
+          char num_str[16];
+          int n = 0;
+          do {
+            int digit = num % 16;
+            if (digit < 10)
+              num_str[n++] = '0' + digit;
+            else
+              num_str[n++] = (*fmt == 'x' ? 'a' : 'A') + (digit - 10);
+            num /= 16;
+          } while (num > 0);
+          int pad = width - n;
+          for (int i = 0; i < pad; i++) {
+            putch(zero_pad ? '0' : ' ');
+            count++;
+          }
+          for (int i = n - 1; i >= 0; i--) {
+            putch(num_str[i]);
+            count++;
+          }
+          break;
+        }
+        case 's': {
           const char *s = va_arg(ap, const char *);
-          while(*s) {
+          int slen = 0;
+          const char *sp = s;
+          while (*sp++) slen++;
+          int pad = width - slen;
+          for (int i = 0; i < pad; i++) {
+            putch(' ');
+            count++;
+          }
+          while (*s) {
             putch(*(s++));
             count++;
           }
+          break;
+        }
+        case '%':
+          putch('%');
+          count++;
           break;
         default:
           putch('%');
@@ -57,9 +137,7 @@ int printf(const char *fmt, ...) {
           count++;
           break;
       }
-    }
-    else 
-    {
+    } else {
       putch(*fmt);
       count++;
     }
