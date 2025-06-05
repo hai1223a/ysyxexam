b9104e560277a7ca5da4f19141869a20edc501b0
tracer-ysyx <tracer@ysyx.org>
2025-06-05 17:25:52 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:25:52 up 1 day, 47 min,  1 user,  load average: 1.37, 0.98, 0.88

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -34,15 +34,15 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  static void *addr;
-  static char zero_;
-  void *temp_addr;
-  if(addr == NULL) addr = heap.start;
-  if(size == 0) return (void *)&zero_;
-  temp_addr = addr;
-  addr += size;
-  if(addr > heap.end) temp_addr = NULL;
-  return temp_addr;
+  // static void *addr;
+  // static char zero_;
+  // void *temp_addr;
+  // if(addr == NULL) addr = heap.start;
+  // if(size == 0) return (void *)&zero_;
+  // temp_addr = addr;
+  // addr += size;
+  // if(addr > heap.end) temp_addr = NULL;
+  // return temp_addr;
 #endif
   return NULL;
 }
