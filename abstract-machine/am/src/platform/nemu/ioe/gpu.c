c547269ee726c33787f196dbae18304a128af682
tracer-ysyx <tracer@ysyx.org>
2025-06-07 12:57:08 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:57:08 up  2:41,  1 user,  load average: 1.02, 0.83, 0.75

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,18 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = 400;  
+  int h = 300;  
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = (int)(uint32_t)inw(VGACTL_ADDR + 0x2), .height = (int)(uint32_t)inw(VGACTL_ADDR),
+    .width = (int)(uint32_t)inw(VGACTL_ADDR + 2), .height = (int)(uint32_t)inw(VGACTL_ADDR),
     .vmemsz = 0
   };
 }
--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -3,11 +3,11 @@
 
 void __am_timer_init() {
   outl(RTC_ADDR, 0);        
-  outl(RTC_ADDR + 0x4, 0);
+  outl(RTC_ADDR + 4, 0);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = inl(RTC_ADDR + 0x4);
+  uptime->us = inl(RTC_ADDR + 4);
   uptime->us <<= 32;
   uptime->us += inl(RTC_ADDR);
 }
