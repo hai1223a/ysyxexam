3746d07b2503060a8dbb91f9c31efa5ed741e790
tracer-ysyx <tracer@ysyx.org>
2025-06-07 14:16:00 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:16:00 up  4:00,  1 user,  load average: 1.19, 1.06, 1.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -21,24 +21,23 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  static uint32_t *pb;
-  static int x,y,w,h,point;
+  uint32_t *pb;
+  int x,y,w,h,point;
+  int i,j;
   int width = (int)(uint32_t)inw(VGACTL_ADDR + 2);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  if (ctl->sync) {
-    int i, j;
-    for(i = 0; i < h; i++) 
-      for(j = 0; j < w; j++) {
-        fb[point + h * width + j] = pb[(i+1)*(j+1)-1];
-      }
-    outl(SYNC_ADDR, 1);
-  } else {
     pb = (uint32_t *)ctl->pixels;
     x = ctl->x;
     y = ctl->y;
     h = ctl->h;
     w = ctl->w;
     point = y * width + x;
+    for(i = 0; i < h; i++) 
+      for(j = 0; j < w; j++) {
+        fb[point + h * width + j] = pb[(i+1)*(j+1)-1];
+      }
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
   }
 }
 // *((uint32_t *)ctl->pixels + ((i + 1) * (j + 1) - 1))
