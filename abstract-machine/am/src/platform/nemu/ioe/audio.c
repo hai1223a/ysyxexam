6bcc8a37743bd79e04f7631d9ac600f2186ac1b6
tracer-ysyx <tracer@ysyx.org>
2025-06-07 19:50:44 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:50:44 up  9:35,  1 user,  load average: 0.88, 0.80, 0.75

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -12,15 +12,36 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  static uintptr_t sbuf_opt = 0;
+  int buf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+  int nwrite = 0;
+  while (nwrite < len) {
+    int count = inl(AUDIO_COUNT_ADDR);
+    int remain = buf_size - count;
+    if(remain > len) remain = len; 
+    for(int i = 0; i < remain; i++) {
+      if(sbuf_opt == buf_size) sbuf_opt = 0;
+      outb(AUDIO_SBUF_ADDR + sbuf_opt, *((uint8_t *)ctl->buf.start + nwrite));
+      sbuf_opt += 1;
+      nwrite += 1;
+    }
+    outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + remain);
+  }
 }
