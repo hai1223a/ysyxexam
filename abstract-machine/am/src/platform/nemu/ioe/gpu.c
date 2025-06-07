63b34c46c57090702eae7074b0763bad3f7ad826
tracer-ysyx <tracer@ysyx.org>
2025-06-07 13:45:48 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:45:48 up  3:30,  1 user,  load average: 0.62, 0.69, 0.73

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -29,7 +29,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     for(i = 0; i < ctl->h; i++) 
       for(j = 0; j < ctl->w; j++) {
-        fb[point + j + i * h] = i*j;
+        fb[point + j + i * h] = 0x00FFFFFF;
       }
     outl(SYNC_ADDR, 1);
   }
