09d53558d04e72438b70b150b36e63d50464c190
tracer-ysyx <tracer@ysyx.org>
2025-06-08 23:15:09 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:15:09 up  7:43,  1 user,  load average: 0.66, 0.73, 0.60

--- npc/obj_dir/Vysyx_25050136_NPC.mk
+++ npc/obj_dir/Vysyx_25050136_NPC.mk
@@ -37,7 +37,7 @@ VM_PREFIX = Vysyx_25050136_NPC
 VM_MODPREFIX = Vysyx_25050136_NPC
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-fsanitize=address -I./include -I./include/generated  \
+	-fsanitize=address -I../include -I../include/generated  \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
