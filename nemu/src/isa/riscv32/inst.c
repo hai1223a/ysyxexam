da9a16e7b46c203a1548de90ba30568b19ead047
tracer-ysyx <tracer@ysyx.org>
2025-06-16 16:38:21 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:38:21 up  1:09,  1 user,  load average: 0.52, 0.63, 0.81

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -160,7 +160,7 @@ extern struct FUNC_FTRACE{
   word_t addr;
   char func_name[16];
 } FUNC_FTRACER[10];
-int FUNC_stack[100] = {0};
+int FUNC_stack[1024] = {0};
 
 static void ftracer_log(Decode *s, int name)
 {
