96e1bfbfa4ed7fe0fc52f84683a16382a8a7bad6
tracer-ysyx <tracer@ysyx.org>
2025-06-16 16:27:04 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:04 up 58 min,  1 user,  load average: 1.63, 1.13, 0.91

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -169,10 +169,10 @@ static void load_elf() {
   free((void *)strtab);
   free(sh_table);
   fclose(file);
-  // for (size_t i = 0; i < 10; i++)
-  // {
-  //   printf("函数名: %s, 函数地址: %8x\n", FUNC_FTRACER[i].func_name, FUNC_FTRACER[i].addr);
-  // }
+  for (size_t i = 0; i < 10; i++)
+  {
+    printf("函数名: %s, 函数地址: %8x\n", FUNC_FTRACER[i].func_name, FUNC_FTRACER[i].addr);
+  }
   
 }
 
