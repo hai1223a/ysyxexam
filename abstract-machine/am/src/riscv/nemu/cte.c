3c868693aa7c8a073bc7120550670a34699d90f8
tracer-ysyx <tracer@ysyx.org>
2025-06-28 23:01:14 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:01:14 up 2 days,  5:51,  1 user,  load average: 1.01, 0.82, 0.70

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,8 +8,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 11: ev.event = EVENT_YIELD; break;
-      default: ev.event = EVENT_ERROR; break;
+      default: ev.event = c->mcause; break;
     }
 
     c = user_handler(ev, c);
