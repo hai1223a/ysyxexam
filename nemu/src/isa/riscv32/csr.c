c07f293dff1289720f2a1ba04045f320ed01a22f
tracer-ysyx <tracer@ysyx.org>
2025-06-28 18:53:38 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-26-generic #26~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr 17 19:20:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:38 up 2 days,  1:44,  1 user,  load average: 0.73, 0.95, 0.75

--- nemu/src/isa/riscv32/csr.c
+++ nemu/src/isa/riscv32/csr.c
@@ -1,8 +1,9 @@
 #include <isa.h>
 #include "local-include/csr.h"
 
-riscv_csr csrs[3] = {
+riscv_csr csrs[4] = {
     {.addr = 0x341, .name = "mepc"},
     {.addr = 0x300, .name = "mstatus"},
-    {.addr = 0x342, .name = "mcause"}
+    {.addr = 0x342, .name = "mcause"},
+    {.addr = 0x305, .name = "mtvec"},
 };
