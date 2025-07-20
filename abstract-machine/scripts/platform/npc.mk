6f6b35f76f05573f565b6fd95dc302c815ec65a8
tracer-ysyx <tracer@ysyx.org>
2025-07-20 20:04:05 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.11.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jun 26 14:16:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:04:05 up  8:57,  1 user,  load average: 1.00, 1.16, 1.23

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -34,12 +34,12 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NPC_HOME) run ARGS="$(NPCFLAGS)"
+	@$(MAKE) -C $(NPC_HOME) run ARGS="$(NPCFLAGS)"
 
 gdb: insert-arg
-	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)"
+	@$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)"
 
 wave: insert-arg
-	$(MAKE) -C $(NPC_HOME) wave ARGS="$(NPCFLAGS)"
+	@$(MAKE) -C $(NPC_HOME) wave ARGS="$(NPCFLAGS)"
 
 .PHONY: insert-arg
