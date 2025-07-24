4e3e173907d75404c788e4e962192bb95d3e0b1a
tracer-ysyx <tracer@ysyx.org>
2025-07-24 11:43:40 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:43:40 up 1 day, 20:58,  1 user,  load average: 0.77, 0.90, 0.66

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -252,7 +252,7 @@ static void ftracer_log(Decode *s, int name)
         }
       }
       if(good_ret) {
-          ftracer_write("\n0x%8x %u R \b[%s @ 0x%8x]",s->pc, p_stack, FUNC_FTRACER[FUNC_stack[p_stack].num].func_name, Reg(1));
+          ftracer_write("\n0x%8x %u R [%s @ 0x%8x]",s->pc, p_stack, FUNC_FTRACER[FUNC_stack[p_stack].num].func_name, Reg(1));
       } else {
         p_stack = p_stack_init;
       }
