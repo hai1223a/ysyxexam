dbf20aaac43622a30112ba60c3c7d716be97c134
tracer-ysyx <tracer@ysyx.org>
2025-07-24 12:15:01 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:15:01 up 1 day, 21:29,  1 user,  load average: 0.58, 0.51, 0.55

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -254,6 +254,7 @@ static void ftracer_log(Decode *s, int name)
         }
       }
       if(good_ret) {
+                if(repeat_FUNC.num) ftracer_write("    重复%d次", repeat_FUNC.num + 1);
         ftracer_write("\n0x%8x %u R [%s @ 0x%8x]",s->pc, p_stack, FUNC_FTRACER[FUNC_stack[p_stack].num].func_name, Reg(1));
         repeat_FUNC.ret_pc = Reg(1);
         repeat_FUNC.call_pc = Reg(1) - 4;
