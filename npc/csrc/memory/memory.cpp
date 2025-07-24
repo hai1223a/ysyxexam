55e5f5546103b3cfe5a16935f1455eb91cfa2cc3
tracer-ysyx <tracer@ysyx.org>
2025-07-24 19:51:21 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:51:21 up  4:06,  1 user,  load average: 0.57, 0.42, 0.30

--- npc/csrc/memory/memory.cpp
+++ npc/csrc/memory/memory.cpp
@@ -11,15 +11,16 @@ long init_pmem(char *img_file)
   {
     uint32_t *pmem_w = (uint32_t *)pmem;
     *pmem_w++ = 0x00000297; // auipc t0,0
-    *pmem_w++ = 0x00028513; // addi a0, t0, 0
-    *pmem_w++ = 0x00100593; // li a1, 1      (addi a1, zero, 1)
-    *pmem_w++ = 0x00b52023; // sw a1, 0(t0)  (访存：将a1写入t0指向的内存)
-    *pmem_w++ = 0x00052283; // lw t0, 0(a0)  (访存：从a0指向的内存读到t0)
-    *pmem_w++ = 0x0040006f; // jal zero, +4  (跳转到下一条，演示jal)
-    *pmem_w++ = 0x00000013; // nop           (addi zero, zero, 0)
-    *pmem_w++ = 0x00128293; // addi t0, t0, 1 (寄存器加法)
-    *pmem_w++ = 0xfe529ee3; // bne a0, a1, -4 (分支跳转：如果a0!=a1则跳回前面)
-    *pmem_w++ = 0x00100073; // ebreak        (用于trap/调试)
+    *pmem_w++ = 0x02428293; // addi t0,t0,36      // t0 = 指令区首地址+36
+    *pmem_w++ = 0x00100513; // li a0,1            // a0 = 1
+    *pmem_w++ = 0x00a2a023; // sw  a0,0(t0)       // [t0+0] = a0 (把1写到数据区)
+    *pmem_w++ = 0x0002a503; // lw  a0,0(t0)       // a0 = [t0+0] (从数据区读回a0)
+    *pmem_w++ = 0x0040006f; // jal zero, +4       // 跳转到下一条（演示jal）
+    *pmem_w++ = 0x00128293; // addi t0,t0,1       // t0 = t0 + 1
+    *pmem_w++ = 0xfe529ae3; // bne a0,a1,-4       // 如果a0!=a1, 跳回前面
+    *pmem_w++ = 0x00100073; // ebreak             // 终止
+    *pmem_w++ = 0xdeadbeef; // 数据区内容
+    *pmem_w++ = 0x12345678; // 数据区内容
     Log("没有给源文件, 程序使用了内置的代码.");
     return 20;
   }
