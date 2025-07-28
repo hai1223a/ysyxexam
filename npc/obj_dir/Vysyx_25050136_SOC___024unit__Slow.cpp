aa04b242471c81836ccf5114e7fd33b9700dd0c9
tracer-ysyx <tracer@ysyx.org>
2025-07-28 18:56:29 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:56:29 up  5:42,  1 user,  load average: 0.63, 0.62, 0.55

---
+++ npc/obj_dir/Vysyx_25050136_SOC___024unit__Slow.cpp
@@ -0,0 +1,24 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vysyx_25050136_SOC.h for the primary calling header
+
+#include "Vysyx_25050136_SOC__pch.h"
+#include "Vysyx_25050136_SOC__Syms.h"
+#include "Vysyx_25050136_SOC___024unit.h"
+
+void Vysyx_25050136_SOC___024unit___ctor_var_reset(Vysyx_25050136_SOC___024unit* vlSelf);
+
+Vysyx_25050136_SOC___024unit::Vysyx_25050136_SOC___024unit(Vysyx_25050136_SOC__Syms* symsp, const char* v__name)
+    : VerilatedModule{v__name}
+    , vlSymsp{symsp}
+ {
+    // Reset structure values
+    Vysyx_25050136_SOC___024unit___ctor_var_reset(this);
+}
+
+void Vysyx_25050136_SOC___024unit::__Vconfigure(bool first) {
+    (void)first;  // Prevent unused variable warning
+}
+
+Vysyx_25050136_SOC___024unit::~Vysyx_25050136_SOC___024unit() {
+}
