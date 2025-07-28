fdb2174d6906cf5733be9d8a35d3f7baee97e68e
tracer-ysyx <tracer@ysyx.org>
2025-07-28 19:08:46 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:08:46 up  5:54,  1 user,  load average: 0.64, 0.67, 0.66

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -1,6 +1,6 @@
 #include "../../include/common.h"
 
-uint32_t pc__ = RESET_VECTOR; // 执行完指令后的PC值
+uint32_t pc__ = 0; // 执行完指令后的PC值
 uint32_t pc_pre = 0;          // 执行的指令的PC值
 uint32_t inst_pre = 0;        // 执行的指令值
 vluint64_t sim_time = 0;      // 记录仿真时间
