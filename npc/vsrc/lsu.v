c2d549935396469c1ba7c6cd41b911e9c538b3af
tracer-ysyx <tracer@ysyx.org>
2025-07-28 21:42:16 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:42:16 up  8:28,  1 user,  load average: 1.28, 0.99, 0.75

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -102,9 +102,9 @@ module ysyx_25050136_LSU
     assign r_fire =m_rvalid_i & m_rready_o;
 
     // 写事务
-    reg m_awvalid_r;
-    reg m_wvalid_r;
-    reg m_bready_r;
+    reg [3:0] m_awvalid_r;
+    reg [4:0] m_wvalid_r;
+    reg [5:0] m_bready_r;
     wire aw_fire, w_fire, b_fire;
 
     always @(posedge clk) begin
@@ -113,12 +113,12 @@ module ysyx_25050136_LSU
             m_wvalid_r <= 0;
         end else begin
             if(aw_fire) begin
-                m_awvalid_r <= 0;                
+                m_awvalid_r <= {m_awvalid_r[2:0], 1'd0};                
             end else begin
                 if(fvalid_i & mem_wen_i) begin
-                    m_awvalid_r <= 1;
+                    m_awvalid_r <= {m_awvalid_r[2:0], 1'd1};     
                 end else begin
-                    m_awvalid_r <= 0;
+                    m_awvalid_r <= {m_awvalid_r[2:0], m_awvalid_r[0]}; 
                 end
             end
             if(w_fire) begin
