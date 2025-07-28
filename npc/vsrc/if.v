f59efc8a6b3062fb80c2821f80c30f37bfe4272a
tracer-ysyx <tracer@ysyx.org>
2025-07-28 20:25:51 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:25:51 up  7:12,  1 user,  load average: 1.19, 0.78, 0.69

--- npc/vsrc/if.v
+++ npc/vsrc/if.v
@@ -71,7 +71,7 @@ module ysyx_25050136_IF
     assign m_araddr_o = pc;
     assign m_rready_o = m_rready_r;
     assign bvalid_o = r_fire & (m_rresp_i == 2'd0);
-    assign inst_o = (m_rdata_i == 0) ? inst_r : m_rdata_i;
+    assign inst_o = m_rdata_i;
     assign ar_fire = m_arvalid_o & m_arready_i;
     assign r_fire = m_rvalid_i & m_rready_o;
 
