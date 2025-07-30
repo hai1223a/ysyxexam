e0311da397b2665a3b077fec139dfd0349065c4e
tracer-ysyx <tracer@ysyx.org>
2025-07-30 21:54:05 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:05 up 36 min,  1 user,  load average: 1.00, 1.12, 0.99

---
+++ npc/vsrc/arbiter.v
@@ -0,0 +1,122 @@
+module ysyx_25050136_ARBITER
+    #(
+        MASTER_NUM = 2,
+        SLAVER_NUM = 1,
+        DATA_WIDTH = 32,
+        ADDR_WIDTH = 32
+     )
+     (
+         // 通用数据
+         input                                          aclk,
+         input                                       aresetn,
+         // 来自主设备的AXI信号
+         // 写地址
+         input     [MASTER_NUM-1:0]              s_awvalid_i,
+         output    [MASTER_NUM-1:0]              s_awready_o,
+         input     [MASTER_NUM*ADDR_WIDTH-1:0]    s_awaddr_i,
+         // 写数据
+         input     [MASTER_NUM-1:0]               s_wvalid_i,
+         output    [MASTER_NUM-1:0]               s_wready_o,
+         input     [MASTER_NUM*DATA_WIDTH-1:0]     s_wdata_i,
+         input     [MASTER_NUM*4-1:0]              s_wstrb_i,
+         // 写响应
+         output    [MASTER_NUM-1:0]               s_bvalid_o,
+         input     [MASTER_NUM-1:0]               s_bready_i,
+         output    [MASTER_NUM*2-1:0]              s_bresp_o,
+         // 读地址
+         input     [MASTER_NUM-1:0]              s_arvalid_i,
+         output    [MASTER_NUM-1:0]              s_arready_o,
+         input     [MASTER_NUM*ADDR_WIDTH-1:0]    s_araddr_i,
+         // 读数据
+         output    [MASTER_NUM-1:0]               s_rvalid_o,
+         input     [MASTER_NUM-1:0]               s_rready_i,
+         output    [MASTER_NUM*DATA_WIDTH-1:0]     s_rdata_o,
+         output    [MASTER_NUM*2-1:0]              s_rresp_o,
+         // 来自从设备的AXI信号
+         // 写地址
+         output    [SLAVER_NUM-1:0]              m_awvalid_o,
+         input     [SLAVER_NUM-1:0]              m_awready_i,
+         output    [SLAVER_NUM*DATA_WIDTH-1:0]    m_awaddr_o,
+         // 写数据             
+         output    [SLAVER_NUM-1:0]               m_wvalid_o,
+         input     [SLAVER_NUM-1:0]               m_wready_i,
+         output    [SLAVER_NUM*DATA_WIDTH-1:0]     m_wdata_o,
+         output    [SLAVER_NUM*4-1:0]              m_wstrb_o,
+         // 写响应               
+         input     [SLAVER_NUM-1:0]               m_bvalid_i,
+         output    [SLAVER_NUM-1:0]               m_bready_o,
+         input     [SLAVER_NUM*2-1:0]              m_bresp_i,
+         // 读地址               
+         output    [SLAVER_NUM-1:0]              m_arvalid_o,
+         input     [SLAVER_NUM-1:0]              m_arready_i,
+         output    [SLAVER_NUM*DATA_WIDTH-1:0]    m_araddr_o,
+         // 读数据                
+         input     [SLAVER_NUM-1:0]               m_rvalid_i,
+         output    [SLAVER_NUM-1:0]               m_rready_o,
+         input     [SLAVER_NUM*DATA_WIDTH-1:0]     m_rdata_i,
+         input     [SLAVER_NUM*2-1:0]              m_rresp_i 
+     );
+    // 目前默认多主一从, 仲裁逻辑为优先级仲裁, 低位优先级最高
+    reg [MASTER_NUM-1:0] current_master;    // 当前主设备
+    reg [MASTER_NUM-1:0] current_id;
+    reg [MASTER_NUM-1:0] master_grand;      // 主设备授权
+    reg [MASTER_NUM-1:0] master_id;
+    wire [MASTER_NUM-1:0] request;          // 主设备请求
+
+    assign request = s_awvalid_i | s_arvalid_i;
+    
+    always @(*) begin
+        master_grand = 0;
+        master_id = 0;
+        for (integer i = 0; i < MASTER_NUM; i = i + 1) begin
+            if(request[i]) begin
+                master_id = i;
+                master_grand[i] = 1'b1;
+                break; 
+            end
+        end
+    end
+
+    always @(posedge aclk) begin
+        if (!aresetn) begin
+            current_master <= 0;
+            current_id <= 0;
+        end else begin
+            if(current_master == 0 || 
+            (m_awvalid_o && m_awready_i) || 
+            (m_arvalid_o && m_arready_i)) begin
+                current_id <= master_id;
+                current_master <= master_grand;
+            end
+        end
+    end
+
+    // 写地址
+    assign m_awvalid_o = |(s_awvalid_i & current_master);
+    assign m_awaddr_o = s_awaddr_i[current_id * ADDR_WIDTH +: ADDR_WIDTH];
+    // 写数据
+    assign m_wvalid_o = |(s_wvalid_i & current_master);
+    assign m_wdata_o = s_wdata_i[current_id * DATA_WIDTH +: DATA_WIDTH];
+    assign m_wstrb_o = s_wstrb_i[current_id * 4 +: 4];
+    // 写响应
+    assign m_bready_o = |(s_bready_i & current_master);
+    // 读地址
+    assign m_arvalid_o = |(s_arvalid_i & current_master);
+    assign m_araddr_o = s_araddr_i[current_id * ADDR_WIDTH +: ADDR_WIDTH];
+    // 读数据
+    assign m_rready_o = |(s_rready_i & current_master);
+    
+    genvar i;
+    generate
+        for (i = 0; i < MASTER_NUM ; i = i + 1) begin
+            assign s_awready_o[i] = m_awready_i & current_master[i];
+            assign s_wready_o[i] = m_wready_i & current_master[i];
+            assign s_bvalid_o[i] = m_bvalid_i & current_master[i];
+            assign s_bresp_o[i*2+:2] = current_master[i] ? m_bresp_i : 0;
+            assign s_arready_o[i] = m_arready_i & current_master[i];
+            assign s_rvalid_o[i] = m_rvalid_i & current_master[i];
+            assign s_rdata_o[i*DATA_WIDTH+:DATA_WIDTH] = current_master[i] ? m_rdata_i : 0;
+            assign s_rresp_o[i*2+:2] = current_master[i] ? m_rresp_i : 0;
+        end
+    endgenerate
+endmodule
--- npc/vsrc/soc.v
+++ npc/vsrc/soc.v
@@ -1,136 +1,218 @@
 module ysyx_25050136_SOC(
-    input                                   clk,
-    input                                 reset
+    input                               clk  ,
+    input                               reset
 );
-localparam TOP_ADDR_WIDTH = 5;
-localparam TOP_DATA_WIDTH = 32;
+    localparam TOP_ADDR_WIDTH   = 5 ;
+    localparam TOP_DATA_WIDTH   = 32;
+    localparam TOP_MASTER_NUM   = 2 ;
+    localparam TOP_SLAVER_NUM   = 1 ;
 
-              
-wire                          inst_arvalid_o;
-wire                          inst_arready_i;
-wire [TOP_DATA_WIDTH-1:0]      inst_araddr_o;
-wire                           inst_rvalid_i;
-wire                           inst_rready_o;
-wire [TOP_DATA_WIDTH-1:0]       inst_rdata_i;
-wire [1:0]                      inst_rresp_i; 
-wire                           mem_awvalid_o;
-wire                           mem_awready_i;
-wire [TOP_DATA_WIDTH-1:0]       mem_awaddr_o;
-wire                            mem_wvalid_o;
-wire                            mem_wready_i;
-wire [TOP_DATA_WIDTH-1:0]        mem_wdata_o;
-wire [3:0]                       mem_wstrb_o;
-wire                            mem_bvalid_i;
-wire                            mem_bready_o;
-wire [1:0]                       mem_bresp_i;           
-wire                           mem_arvalid_o;
-wire                           mem_arready_i;
-wire [TOP_DATA_WIDTH-1:0]       mem_araddr_o;             
-wire                            mem_rvalid_i;
-wire                            mem_rready_o;
-wire [TOP_DATA_WIDTH-1:0]        mem_rdata_i;
-wire [1:0]                       mem_rresp_i;
+    // 主设备信号        
+    wire                                inst_arvalid_o ;
+    wire                                inst_arready_i ;
+    wire [TOP_DATA_WIDTH-1:0]           inst_araddr_o  ;
+    wire                                inst_rvalid_i  ;
+    wire                                inst_rready_o  ;
+    wire [TOP_DATA_WIDTH-1:0]           inst_rdata_i   ;
+    wire [1:0]                          inst_rresp_i   ; 
+    wire                                inst_awvalid_o ;
+    wire                                inst_awready_i ;
+    wire [TOP_DATA_WIDTH-1:0]           inst_awaddr_o  ;
+    wire                                inst_wvalid_o  ;
+    wire                                inst_wready_i  ;
+    wire [TOP_DATA_WIDTH-1:0]           inst_wdata_o   ;
+    wire [3:0]                          inst_wstrb_o   ;
+    wire                                inst_bvalid_i  ;
+    wire                                inst_bready_o  ;
+    wire [1:0]                          inst_bresp_i   ;
+    wire                                mem_awvalid_o  ;
+    wire                                mem_awready_i  ;
+    wire [TOP_DATA_WIDTH-1:0]           mem_awaddr_o   ;
+    wire                                mem_wvalid_o   ;
+    wire                                mem_wready_i   ;
+    wire [TOP_DATA_WIDTH-1:0]           mem_wdata_o    ;
+    wire [3:0]                          mem_wstrb_o    ;
+    wire                                mem_bvalid_i   ;
+    wire                                mem_bready_o   ;
+    wire [1:0]                          mem_bresp_i    ;           
+    wire                                mem_arvalid_o  ;
+    wire                                mem_arready_i  ;
+    wire [TOP_DATA_WIDTH-1:0]           mem_araddr_o   ;             
+    wire                                mem_rvalid_i   ;
+    wire                                mem_rready_o   ;
+    wire [TOP_DATA_WIDTH-1:0]           mem_rdata_i    ;
+    wire [1:0]                          mem_rresp_i    ;
 
-ysyx_25050136_NPC #(
-    .ADDR_WIDTH(TOP_ADDR_WIDTH),
-    .DATA_WIDTH(TOP_DATA_WIDTH)
-)
-u_ysyx_25050136_NPC(
-    .clk            	(clk             ),
-    .reset          	(reset           ),
-    .inst_arvalid_o 	(inst_arvalid_o  ),
-    .inst_arready_i 	(inst_arready_i  ),
-    .inst_araddr_o  	(inst_araddr_o   ),
-    .inst_rvalid_i  	(inst_rvalid_i   ),
-    .inst_rready_o  	(inst_rready_o   ),
-    .inst_rdata_i   	(inst_rdata_i    ),
-    .inst_rresp_i   	(inst_rresp_i    ),
-    .mem_awvalid_o  	(mem_awvalid_o   ),
-    .mem_awready_i  	(mem_awready_i   ),
-    .mem_awaddr_o   	(mem_awaddr_o    ),
-    .mem_wvalid_o   	(mem_wvalid_o    ),
-    .mem_wready_i   	(mem_wready_i    ),
-    .mem_wdata_o    	(mem_wdata_o     ),
-    .mem_wstrb_o    	(mem_wstrb_o     ),
-    .mem_bvalid_i   	(mem_bvalid_i    ),
-    .mem_bready_o   	(mem_bready_o    ),
-    .mem_bresp_i    	(mem_bresp_i     ),
-    .mem_arvalid_o  	(mem_arvalid_o   ),
-    .mem_arready_i  	(mem_arready_i   ),
-    .mem_araddr_o   	(mem_araddr_o    ),
-    .mem_rvalid_i   	(mem_rvalid_i    ),
-    .mem_rready_o   	(mem_rready_o    ),
-    .mem_rdata_i    	(mem_rdata_i     ),
-    .mem_rresp_i    	(mem_rresp_i     )
-);
+    // 仲裁器信号
+    wire [TOP_MASTER_NUM-1:0]                s_awvalid_i ;
+    wire [TOP_MASTER_NUM-1:0]                s_awready_o ;
+    wire [TOP_MASTER_NUM*TOP_DATA_WIDTH-1:0] s_awaddr_i  ;
+    wire [TOP_MASTER_NUM-1:0]                s_wvalid_i  ;
+    wire [TOP_MASTER_NUM-1:0]                s_wready_o  ;
+    wire [TOP_MASTER_NUM*TOP_DATA_WIDTH-1:0] s_wdata_i   ;
+    wire [TOP_MASTER_NUM*4-1:0]              s_wstrb_i   ;
+    wire [TOP_MASTER_NUM-1:0]                s_bvalid_o  ;
+    wire [TOP_MASTER_NUM-1:0]                s_bready_i  ;
+    wire [TOP_MASTER_NUM*2-1:0]              s_bresp_o   ;
+    wire [TOP_MASTER_NUM-1:0]                s_arvalid_i ;
+    wire [TOP_MASTER_NUM-1:0]                s_arready_o ;
+    wire [TOP_MASTER_NUM*TOP_DATA_WIDTH-1:0] s_araddr_i  ;
+    wire [TOP_MASTER_NUM-1:0]                s_rvalid_o  ;
+    wire [TOP_MASTER_NUM-1:0]                s_rready_i  ;
+    wire [TOP_MASTER_NUM*TOP_DATA_WIDTH-1:0] s_rdata_o   ;
+    wire [TOP_MASTER_NUM*2-1:0]              s_rresp_o   ;
+    wire [1-1:0]                             m_awvalid_o ;
+    wire [1-1:0]                             m_awready_i ;
+    wire [1*TOP_DATA_WIDTH-1:0]              m_awaddr_o  ;
+    wire [1-1:0]                             m_wvalid_o  ;
+    wire [1-1:0]                             m_wready_i  ;
+    wire [1*TOP_DATA_WIDTH-1:0]              m_wdata_o   ;
+    wire [1*4-1:0]                           m_wstrb_o   ;
+    wire [1-1:0]                             m_bvalid_i  ;
+    wire [1-1:0]                             m_bready_o  ;
+    wire [1*2-1:0]                           m_bresp_i   ;
+    wire [1-1:0]                             m_arvalid_o ;
+    wire [1-1:0]                             m_arready_i ;
+    wire [1*TOP_DATA_WIDTH-1:0]              m_araddr_o  ;
+    wire [1-1:0]                             m_rvalid_i  ;
+    wire [1-1:0]                             m_rready_o  ;
+    wire [1*TOP_DATA_WIDTH-1:0]              m_rdata_i   ;
+    wire [1*2-1:0]                           m_rresp_i   ;
 
-wire                           inst_awvalid_o;
-wire                           inst_awready_i;
-wire [TOP_DATA_WIDTH-1:0]       inst_awaddr_o;
-wire                            inst_wvalid_o;
-wire                            inst_wready_i;
-wire [TOP_DATA_WIDTH-1:0]        inst_wdata_o;
-wire [3:0]                       inst_wstrb_o;
-wire                            inst_bvalid_i;
-wire                            inst_bready_o;
-wire [1:0]                       inst_bresp_i;
+    // 指令端口默认连接
+    assign inst_awvalid_o   = 0 ;
+    assign inst_awaddr_o    = 0 ;
+    assign inst_wvalid_o    = 0 ;
+    assign inst_wdata_o     = 0 ;
+    assign inst_wstrb_o     = 0 ;
+    assign inst_bready_o    = 0 ;
 
-assign inst_awvalid_o = 0;
-assign inst_awaddr_o = 0;
-assign inst_wvalid_o = 0;
-assign inst_wdata_o = 0;
-assign inst_wstrb_o = 0;
-assign inst_bready_o = 0;
-ysyx_25050136_SRAM #(
-    .ADDR_WIDTH(TOP_DATA_WIDTH),
-    .DATA_WIDTH(TOP_DATA_WIDTH)
-)
-u_ROM(
-    .aclk        	(clk             ),
-    .aresetn     	(~reset          ),
-    .s_awvalid_i 	(inst_awvalid_o  ),
-    .s_awready_o 	(inst_awready_i  ),
-    .s_awaddr_i  	(inst_awaddr_o   ),
-    .s_wvalid_i  	(inst_wvalid_o   ),
-    .s_wready_o  	(inst_wready_i   ),
-    .s_wdata_i   	(inst_wdata_o    ),
-    .s_wstrb_i   	(inst_wstrb_o    ),
-    .s_bvalid_o  	(inst_bvalid_i   ),
-    .s_bready_i  	(inst_bready_o   ),
-    .s_bresp_o   	(inst_bresp_i    ),
-    .s_arvalid_i 	(inst_arvalid_o  ),
-    .s_arready_o 	(inst_arready_i  ),
-    .s_araddr_i  	(inst_araddr_o   ),
-    .s_rvalid_o  	(inst_rvalid_i   ),
-    .s_rready_i  	(inst_rready_o   ),
-    .s_rdata_o   	(inst_rdata_i    ),
-    .s_rresp_o   	(inst_rresp_i    )
-);
+    // 主设备到仲裁器信号连接
+    assign s_awvalid_i      = {mem_awvalid_o, inst_awvalid_o};
+    assign s_awaddr_i       = {mem_awaddr_o,  inst_awaddr_o} ;
+    assign s_wvalid_i       = {mem_wvalid_o,  inst_wvalid_o} ;
+    assign s_wdata_i        = {mem_wdata_o,   inst_wdata_o}  ;
+    assign s_wstrb_i        = {mem_wstrb_o,   inst_wstrb_o}  ;
+    assign s_bready_i       = {mem_bready_o,  inst_bready_o} ;
+    assign s_arvalid_i      = {mem_arvalid_o, inst_arvalid_o};
+    assign s_araddr_i       = {mem_araddr_o,  inst_araddr_o} ;
+    assign s_rready_i       = {mem_rready_o,  inst_rready_o} ;
 
-ysyx_25050136_SRAM #(
-    .ADDR_WIDTH(TOP_DATA_WIDTH),
-    .DATA_WIDTH(TOP_DATA_WIDTH)
-)
-u_RAM(
-    .aclk        	(clk         ),
-    .aresetn     	(~reset      ),
-    .s_awvalid_i 	(mem_awvalid_o  ),
-    .s_awready_o 	(mem_awready_i  ),
-    .s_awaddr_i  	(mem_awaddr_o   ),
-    .s_wvalid_i  	(mem_wvalid_o   ),
-    .s_wready_o  	(mem_wready_i   ),
-    .s_wdata_i   	(mem_wdata_o    ),
-    .s_wstrb_i   	(mem_wstrb_o    ),
-    .s_bvalid_o  	(mem_bvalid_i   ),
-    .s_bready_i  	(mem_bready_o   ),
-    .s_bresp_o   	(mem_bresp_i    ),
-    .s_arvalid_i 	(mem_arvalid_o  ),
-    .s_arready_o 	(mem_arready_i  ),
-    .s_araddr_i  	(mem_araddr_o   ),
-    .s_rvalid_o  	(mem_rvalid_i   ),
-    .s_rready_i  	(mem_rready_o   ),
-    .s_rdata_o   	(mem_rdata_i    ),
-    .s_rresp_o   	(mem_rresp_i    )
-);
+    // 仲裁器到主设备信号连接
+    assign inst_awready_i   = s_awready_o[0]  ;
+    assign mem_awready_i    = s_awready_o[1]  ;
+    assign inst_wready_i    = s_wready_o[0]   ;
+    assign mem_wready_i     = s_wready_o[1]   ;
+    assign inst_bvalid_i    = s_bvalid_o[0]   ;
+    assign mem_bvalid_i     = s_bvalid_o[1]   ;
+    assign inst_bresp_i     = s_bresp_o[1:0]  ;
+    assign mem_bresp_i      = s_bresp_o[3:2]  ;
+    assign inst_arready_i   = s_arready_o[0]  ;
+    assign mem_arready_i    = s_arready_o[1]  ;
+    assign inst_rvalid_i    = s_rvalid_o[0]   ;
+    assign mem_rvalid_i     = s_rvalid_o[1]   ;
+    assign inst_rdata_i     = s_rdata_o[0+:TOP_DATA_WIDTH] ;
+    assign mem_rdata_i      = s_rdata_o[TOP_DATA_WIDTH+:TOP_DATA_WIDTH] ;
+    assign inst_rresp_i     = s_rresp_o[1:0]  ;
+    assign mem_rresp_i      = s_rresp_o[3:2]  ;
+
+    // NPC模块实例化
+    ysyx_25050136_NPC #(
+        .ADDR_WIDTH(TOP_ADDR_WIDTH),
+        .DATA_WIDTH(TOP_DATA_WIDTH)
+    ) u_ysyx_25050136_NPC (
+        .clk            (clk            ),
+        .reset          (reset          ),
+        .inst_arvalid_o (inst_arvalid_o ),
+        .inst_arready_i (inst_arready_i ),
+        .inst_araddr_o  (inst_araddr_o  ),
+        .inst_rvalid_i  (inst_rvalid_i  ),
+        .inst_rready_o  (inst_rready_o  ),
+        .inst_rdata_i   (inst_rdata_i   ),
+        .inst_rresp_i   (inst_rresp_i   ),
+        .mem_awvalid_o  (mem_awvalid_o  ),
+        .mem_awready_i  (mem_awready_i  ),
+        .mem_awaddr_o   (mem_awaddr_o   ),
+        .mem_wvalid_o   (mem_wvalid_o   ),
+        .mem_wready_i   (mem_wready_i   ),
+        .mem_wdata_o    (mem_wdata_o    ),
+        .mem_wstrb_o    (mem_wstrb_o    ),
+        .mem_bvalid_i   (mem_bvalid_i   ),
+        .mem_bready_o   (mem_bready_o   ),
+        .mem_bresp_i    (mem_bresp_i    ),
+        .mem_arvalid_o  (mem_arvalid_o  ),
+        .mem_arready_i  (mem_arready_i  ),
+        .mem_araddr_o   (mem_araddr_o   ),             
+        .mem_rvalid_i   (mem_rvalid_i   ),
+        .mem_rready_o   (mem_rready_o   ),
+        .mem_rdata_i    (mem_rdata_i    ),
+        .mem_rresp_i    (mem_rresp_i    )
+    );
+
+    // 仲裁器模块实例化
+    ysyx_25050136_ARBITER u_ysyx_25050136_ARBITER (
+        .aclk        (clk         ),
+        .aresetn     (~reset      ),
+        .s_awvalid_i (s_awvalid_i ),
+        .s_awready_o (s_awready_o ),
+        .s_awaddr_i  (s_awaddr_i  ),
+        .s_wvalid_i  (s_wvalid_i  ),
+        .s_wready_o  (s_wready_o  ),
+        .s_wdata_i   (s_wdata_i   ),
+        .s_wstrb_i   (s_wstrb_i   ),
+        .s_bvalid_o  (s_bvalid_o  ),
+        .s_bready_i  (s_bready_i  ),
+        .s_bresp_o   (s_bresp_o   ),
+        .s_arvalid_i (s_arvalid_i ),
+        .s_arready_o (s_arready_o ),
+        .s_araddr_i  (s_araddr_i  ),
+        .s_rvalid_o  (s_rvalid_o  ),
+        .s_rready_i  (s_rready_i  ),
+        .s_rdata_o   (s_rdata_o   ),
+        .s_rresp_o   (s_rresp_o   ),
+        .m_awvalid_o (m_awvalid_o ),
+        .m_awready_i (m_awready_i ),
+        .m_awaddr_o  (m_awaddr_o  ),
+        .m_wvalid_o  (m_wvalid_o  ),
+        .m_wready_i  (m_wready_i  ),
+        .m_wdata_o   (m_wdata_o   ),
+        .m_wstrb_o   (m_wstrb_o   ),
+        .m_bvalid_i  (m_bvalid_i  ),
+        .m_bready_o  (m_bready_o  ),
+        .m_bresp_i   (m_bresp_i   ),
+        .m_arvalid_o (m_arvalid_o ),
+        .m_arready_i (m_arready_i ),
+        .m_araddr_o  (m_araddr_o  ),
+        .m_rvalid_i  (m_rvalid_i  ),
+        .m_rready_o  (m_rready_o  ),
+        .m_rdata_i   (m_rdata_i   ),
+        .m_rresp_i   (m_rresp_i   )
+    );
+
+    // SRAM模块实例化
+    ysyx_25050136_SRAM #(
+        .ADDR_WIDTH(TOP_DATA_WIDTH),
+        .DATA_WIDTH(TOP_DATA_WIDTH)
+    ) u_SRAM (
+        .aclk        (clk         ),
+        .aresetn     (~reset      ),
+        .s_awvalid_i (m_awvalid_o ),
+        .s_awready_o (m_awready_i ),
+        .s_awaddr_i  (m_awaddr_o  ),
+        .s_wvalid_i  (m_wvalid_o  ),
+        .s_wready_o  (m_wready_i  ),
+        .s_wdata_i   (m_wdata_o   ),
+        .s_wstrb_i   (m_wstrb_o   ),
+        .s_bvalid_o  (m_bvalid_i  ),
+        .s_bready_i  (m_bready_o  ),
+        .s_bresp_o   (m_bresp_i   ),
+        .s_arvalid_i (m_arvalid_o ),
+        .s_arready_o (m_arready_i ),
+        .s_araddr_i  (m_araddr_o  ),
+        .s_rvalid_o  (m_rvalid_i  ),
+        .s_rready_i  (m_rready_o  ),
+        .s_rdata_o   (m_rdata_i   ),
+        .s_rresp_o   (m_rresp_i   )
+    );
 
 endmodule
