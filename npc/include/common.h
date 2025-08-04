7fe272cd9993f7f99a52ba4194eff03874d33888
tracer-ysyx <tracer@ysyx.org>
2025-08-04 18:57:58 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:58 up 2 days,  7:10,  1 user,  load average: 1.48, 1.28, 1.01

--- npc/include/common.h
+++ npc/include/common.h
@@ -4,7 +4,7 @@
 // 头文件
 //=====================================================
 #include "VysyxSoCFull.h"  // 包含Verilog工程的C++模型
-#include "Vysyx_25050136_SOC___024root.h"
+#include "VysyxSoCFull___024root.h"
 #include <verilated.h>           // Verilator的库
 #include "generated/autoconf.h"
 #ifdef CONFIG_FST
