a42e8085947ed2381deaed92096c8216629912ab
tracer-ysyx <tracer@ysyx.org>
2025-08-09 13:59:03 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:59:03 up 18:57,  1 user,  load average: 0.54, 0.60, 0.69

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -2,7 +2,7 @@ AM_SRCS := riscv/ysyxsoc/start.S \
            riscv/ysyxsoc/trm.c \
 
 CFLAGS    += -fdata-sections -ffunction-sections
-LDSCRIPTS += $(AM_HOME)/am/src/riscv/ysyxsoc/linker.ld
+LDSCRIPTS += $(AM_HOME)/scripts/linker_soc.ld
 LDFLAGS   += --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 IMAGE_NAME = $(basename $(notdir $(IMAGE)))
