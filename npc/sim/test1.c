b2a42bddcf0fe4f4f243c5461650350cf2473364
tracer-ysyx <tracer@ysyx.org>
2025-08-09 17:58:14 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:58:14 up 22:56,  1 user,  load average: 0.59, 0.81, 0.76

--- npc/sim/test1.c
+++ npc/sim/test1.c
@@ -1,5 +1,5 @@
 #define UART_BASE 0x10000000
-#define UART_TX   2
+#define UART_TX   0
 void _start() {
   *(volatile char *)(UART_BASE + UART_TX) = 'A';
   *(volatile char *)(UART_BASE + UART_TX) = '\n';
