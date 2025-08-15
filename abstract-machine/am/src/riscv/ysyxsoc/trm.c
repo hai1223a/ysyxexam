6fe2e32b437dab3245ef7f92b017e2a58f265944
tracer-ysyx <tracer@ysyx.org>
2025-08-15 23:20:58 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:58 up  8:02,  1 user,  load average: 1.59, 1.16, 1.13

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -14,7 +14,8 @@ int count = 0;
 void putch(char ch) {
   while(1) {
     if(count < 16) {
-      // outb(UART_PORT, ch);
+      outb(UART_PORT, ch);
+      count++;
       break;
     }
     if(inb(UART_LSR) & 0b00100000) {
