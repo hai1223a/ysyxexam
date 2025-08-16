3c65c069d9538cf2be2c4478c6e56943e67f7732
tracer-ysyx <tracer@ysyx.org>
2025-08-16 22:14:36 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:36 up 1 day,  6:55,  1 user,  load average: 0.98, 0.92, 0.84

--- abstract-machine/am/src/riscv/ysyxsoc/flash.c
+++ abstract-machine/am/src/riscv/ysyxsoc/flash.c
@@ -2,7 +2,7 @@
 #include "ysyxsoc.h"
 
 int flash_config() {
-  outw(SPI_TX0, 0xf2);  //11110010
+  outw(SPI_TX0, 0x12);  //11110010
   outw(SPI_DIV, 1);
   outw(SPI_SS, 0x80);
   outw(SPI_CTRL, 0x910);
