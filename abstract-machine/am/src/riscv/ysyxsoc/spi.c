c8533845ea435162cc75314f56ea89c4263bcf62
tracer-ysyx <tracer@ysyx.org>
2025-08-18 00:38:58 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:38:58 up 2 days,  9:20,  1 user,  load average: 0.92, 0.56, 0.37

--- abstract-machine/am/src/riscv/ysyxsoc/spi.c
+++ abstract-machine/am/src/riscv/ysyxsoc/spi.c
@@ -22,7 +22,7 @@ uint32_t bitrev_read(uint32_t data) {
 
 uint32_t flash_read(uint32_t data) {
   // uint32_t TX_data = 0x3000000 | (data & 0xffffff);
-  uint32_t TX_data = 0xff2211c0;
+  uint32_t TX_data = 0x032211c0;
   outl(SPI_TX1, TX_data);
   uint32_t ctrl_statu = inw(SPI_CTRL) | 0x100;  
   outw(SPI_CTRL, ctrl_statu);
