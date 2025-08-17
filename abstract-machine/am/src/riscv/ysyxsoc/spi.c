d5db252e023c06c21f98ca7a01e6d60024c7bc23
tracer-ysyx <tracer@ysyx.org>
2025-08-17 23:29:21 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:29:21 up 2 days,  8:10,  1 user,  load average: 0.47, 0.82, 0.86

--- abstract-machine/am/src/riscv/ysyxsoc/spi.c
+++ abstract-machine/am/src/riscv/ysyxsoc/spi.c
@@ -2,7 +2,7 @@
 #include "ysyxsoc.h"
 
 void spi_config(uint32_t SS, uint32_t CHAR_LEN) {
-  uint32_t ctrl_statu = 0x2800 | CHAR_LEN;
+  uint32_t ctrl_statu = 0x2000 | CHAR_LEN;
   outw(SPI_DIV, 3);
   outw(SPI_SS, SS);
   outw(SPI_CTRL, ctrl_statu);
