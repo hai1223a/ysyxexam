db69c9ac06c5f6a1916311c9dcb2fd4f1f433522
tracer-ysyx <tracer@ysyx.org>
2025-08-19 15:46:41 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:41 up  4:02,  1 user,  load average: 0.47, 0.32, 0.38

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -4,6 +4,6 @@
  * NPC Configuration Menu
  *
  */
-#define CONFIG_MSIZE 0x1000
+#define CONFIG_MSIZE 0x10000000
 #define CONFIG_FST 1
-#define CONFIG_MBASE 0x20000000
+#define CONFIG_MBASE 0x30000000
