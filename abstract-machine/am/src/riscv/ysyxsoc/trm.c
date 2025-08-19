487d6121f137fd2c48f94f00e375b7499039ae02
tracer-ysyx <tracer@ysyx.org>
2025-08-19 16:49:13 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:49:13 up  5:04,  1 user,  load average: 1.27, 1.28, 0.97

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -40,19 +40,19 @@ void _uart_init() {
 }
 
 static void _id_puts() {
-  uint32_t mvendorid, marchid;
-  asm volatile ("csrr %0, mvendorid" : "=r"(mvendorid));
-  asm volatile ("csrr %0, marchid" : "=r"(marchid));
-  for (int i = 7; i >= 0; i--) { 
-      uint8_t nibble = (mvendorid >> (i * 4)) & 0xF;
-      putch(nibble < 10 ? '0' + nibble : 'A' + nibble - 10);
-  }
-  putch('\n'  );
-  for (int i = 7; i >= 0; i--) {  
-      uint8_t nibble = (marchid >> (i * 4)) & 0xF;
-      putch(nibble < 10 ? '0' + nibble : 'A' + nibble - 10);
-  }
-  putch('\n'  );
+  // uint32_t mvendorid, marchid;
+  // asm volatile ("csrr %0, mvendorid" : "=r"(mvendorid));
+  // asm volatile ("csrr %0, marchid" : "=r"(marchid));
+  // for (int i = 7; i >= 0; i--) { 
+  //     uint8_t nibble = (mvendorid >> (i * 4)) & 0xF;
+  //     putch(nibble < 10 ? '0' + nibble : 'A' + nibble - 10);
+  // }
+  // putch('\n'  );
+  // for (int i = 7; i >= 0; i--) {  
+  //     uint8_t nibble = (marchid >> (i * 4)) & 0xF;
+  //     putch(nibble < 10 ? '0' + nibble : 'A' + nibble - 10);
+  // }
+  // putch('\n'  );
 }
 
 void _trm_init() {
