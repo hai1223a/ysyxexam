c027c5bef27b176b5e856c9250c9d2abca557b89
tracer-ysyx <tracer@ysyx.org>
2025-08-19 14:47:39 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:39 up  3:03,  1 user,  load average: 1.14, 0.83, 0.76

--- npc/vsrc/perip/spi/rtl/spi_top_apb.v
+++ npc/vsrc/perip/spi/rtl/spi_top_apb.v
@@ -194,7 +194,7 @@ always @(posedge clock) begin
   end else begin
     case (spi_state)
         SPI_IDLE: begin
-          if(state != IDLE) begin
+          if(in_flash & in_psel) begin
             spi_state <= SPI_SETUP;
           end 
         end
