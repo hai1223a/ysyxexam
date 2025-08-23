52adda44c10615c0258a8ff13927a6edae213c77
tracer-ysyx <tracer@ysyx.org>
2025-08-23 23:16:38 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:16:38 up 4 days, 11:32,  1 user,  load average: 1.27, 1.31, 1.16

--- nemu/src/isa/riscv32/csr.c
+++ nemu/src/isa/riscv32/csr.c
@@ -6,6 +6,6 @@ riscv_csr csrs[6] = {
     {.addr = 0x342, .name = "mcause"},
     {.addr = 0x305, .name = "mtvec"},
     {.addr = 0x300, .name = "mstatus"},
-    {.addr = 0xf11, .name = "mvendorid"},
-    {.addr = 0xf12, .name = "marchid"},
+    {.addr = 0xf11, .name = "mvendorid", .value = 0x79737978},
+    {.addr = 0xf12, .name = "marchid", .value = 25050136},
 };
