53be6807a148816979b576dc9b38a6eabd01af1d
tracer-ysyx <tracer@ysyx.org>
2025-08-28 22:17:29 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:29 up  9:57,  1 user,  load average: 1.76, 0.94, 0.86

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -19,19 +19,29 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-    0x00000297, // auipc t0,0
-    0x02428293, // addi t0,t0,36     // t0 = 指令区首地址+36
-    0x00100513, // li a0,1            // a0 = 1
-    0x00a2a023, // sw  a0,0(t0)       // [t0+0] = a0 (把1写到数据区)
-    0x0002a503, // lw  a0,0(t0)       // a0 = [t0+0] (从数据区读回a0)
-    0x0040006f, // jal zero, +4       // 跳转到下一条（演示jal）
-    0x00128293, // addi t0,t0,1       // t0 = t0 + 1
-    0xfe529ae3, // bne a0,a1,-4       // 如果a0!=a1, 跳回前面
-    0x00100073, // ebreak             // 终止
-    0xdeadbeef, // 数据区内容
-    0x12345678, // 数据区内容
+    0xa00012b7, // auipc t0,0
+    0x11428293, // addi t0,t0,36     // t0 = 指令区首地址+36
+    0x12345337, // li a0,1            // a0 = 1
+    0x67830313, // sw  a0,0(t0)       // [t0+0] = a0 (把1写到数据区)
+    0x0062a023, // lw  a0,0(t0)       // a0 = [t0+0] (从数据区读回a0)
+    0x00629023, // jal zero, +4       // 跳转到下一条（演示jal）
+    0x00628023, // addi t0,t0,1       // t0 = t0 + 1
+    0x0002a383, // bne a0,a1,-4       // 如果a0!=a1, 跳回前面
+    0x00029383, // ebreak             // 终止
+    0x00028383, // 数据区内容
+    0x00100073, // 数据区内容
   };
-
+      // *imem_w++ = 0xa00012b7; // lui	t0,0xa0001
+      // *imem_w++ = 0x11428293; // addi	t0,t0,276 
+      // *imem_w++ = 0x12345337; // lui	t1,0x12345
+      // *imem_w++ = 0x67830313; // addi	t1,t1,1656
+      // *imem_w++ = 0x0062a023; // sw	t1,0(t0)
+      // *imem_w++ = 0x00629023; // sh	t1,0(t0)
+      // *imem_w++ = 0x00628023; // sb	t1,0(t0)
+      // *imem_w++ = 0x0002a383; // lw	t2,0(t0)
+      // *imem_w++ = 0x00029383; // lh	t2,0(t0)
+      // *imem_w++ = 0x00028383; // lb	t2,0(t0)
+      // *imem_w++ = 0x00100073; // ebreak 
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
