be14772e087776a879bd4d6b9b7002168f0ec484
tracer-ysyx <tracer@ysyx.org>
2025-08-29 00:05:35 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:05:35 up 11:45,  1 user,  load average: 0.98, 0.83, 0.44

--- npc/csrc/memory/memory.cpp
+++ npc/csrc/memory/memory.cpp
@@ -30,16 +30,13 @@ long init_imem(char *img_file)
   //  *imem_w++ = 0x0047a603; // lw	a2,4(a5) # 2000004
   //  *imem_w++ = 0x00072583; // lw	a1,0(a4) # 2000000 
   //  *imem_w++ = 0x00100073; // ebreak 
-      *imem_w++ = 0xa00012b7; // lui	t0,0xa0001
-      *imem_w++ = 0x11428293; // addi	t0,t0,276 
+      *imem_w++ = 0xa00002b7; // lui	t0,0xa0000
       *imem_w++ = 0x12345337; // lui	t1,0x12345
       *imem_w++ = 0x67830313; // addi	t1,t1,1656
-      *imem_w++ = 0x0062a023; // sw	t1,0(t0)
-      *imem_w++ = 0x00629023; // sh	t1,0(t0)
-      *imem_w++ = 0x00628023; // sb	t1,0(t0)
-      *imem_w++ = 0x0002a383; // lw	t2,0(t0)
-      *imem_w++ = 0x00029383; // lh	t2,0(t0)
-      *imem_w++ = 0x00028383; // lb	t2,0(t0)
+      *imem_w++ = 0x00629123; // sh	t1,2(t0) 
+      *imem_w++ = 0x00629223; // sh	t1,4(t0)
+      *imem_w++ = 0x00229503; // lh	a0,2(t0)
+      *imem_w++ = 0x00429583; // lh	a1,4(t0)
       *imem_w++ = 0x00100073; // ebreak 
       Log("没有给源文件, 程序使用了内置的代码.");
     return 20;
