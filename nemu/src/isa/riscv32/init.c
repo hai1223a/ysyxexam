7725e09a2c6d05975d60169f2ff9afc393e86305
tracer-ysyx <tracer@ysyx.org>
2025-08-29 00:24:57 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:24:57 up 12:05,  1 user,  load average: 0.56, 0.45, 0.43

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -19,14 +19,14 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-    0x00000297, // auipc t0,0
-    0x02428293, // addi t0,t0,36     // t0 = 指令区首地址+36
-    0x00100513, // li a0,1            // a0 = 1
-    0x00a2a023, // sw  a0,0(t0)       // [t0+0] = a0 (把1写到数据区)
-    0x0002a503, // lw  a0,0(t0)       // a0 = [t0+0] (从数据区读回a0)
-    0x0040006f, // jal zero, +4       // 跳转到下一条（演示jal）
-    0x00128293, // addi t0,t0,1       // t0 = t0 + 1
-    0xfe529ae3, // bne a0,a1,-4       // 如果a0!=a1, 跳回前面
+    0xa00002b7, // auipc t0,0
+    0x12345337, // addi t0,t0,36     // t0 = 指令区首地址+36
+    0x67830313, // li a0,1            // a0 = 1
+    0x00629123, // sw  a0,0(t0)       // [t0+0] = a0 (把1写到数据区)
+    0x00629123, // lw  a0,0(t0)       // a0 = [t0+0] (从数据区读回a0)
+    0x00629223, // jal zero, +4       // 跳转到下一条（演示jal）
+    0x00229503, // addi t0,t0,1       // t0 = t0 + 1
+    0x00429583, // bne a0,a1,-4       // 如果a0!=a1, 跳回前面
     0x00100073, // ebreak             // 终止
     0xdeadbeef, // 数据区内容
     0x12345678, // 数据区内容
