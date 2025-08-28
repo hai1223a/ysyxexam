3ccd33ab5638ef840d298329fd84c97cd2426b25
tracer-ysyx <tracer@ysyx.org>
2025-08-28 15:38:49 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:38:49 up  3:19,  1 user,  load average: 0.39, 0.52, 0.50

--- npc/csrc/memory/memory.cpp
+++ npc/csrc/memory/memory.cpp
@@ -30,7 +30,6 @@ long init_imem(char *img_file)
   //  *imem_w++ = 0x0047a603; // lw	a2,4(a5) # 2000004
   //  *imem_w++ = 0x00072583; // lw	a1,0(a4) # 2000000 
   //  *imem_w++ = 0x00100073; // ebreak 
-      *imem_w++ = 0x0000006f; // j 30000000
       *imem_w++ = 0xa00002b7; // lui	t0,0xa0000
       *imem_w++ = 0x00428293; // addi	t0,t0,4
       *imem_w++ = 0x12345337; // lui	t1,0x12345
