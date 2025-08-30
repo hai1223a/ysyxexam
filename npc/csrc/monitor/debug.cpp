afd210d7ee9a37768161ea5b5bbbf267da548440
tracer-ysyx <tracer@ysyx.org>
2025-08-30 21:37:42 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:42 up  4:09,  1 user,  load average: 0.42, 0.57, 0.55

--- npc/csrc/monitor/debug.cpp
+++ npc/csrc/monitor/debug.cpp
@@ -86,7 +86,7 @@ static void init_verilator(int argc, char *argv[])
   // 采样深度为5
   top->trace(tfp, 10);
   // 打开波形文件
-  tfp->open("build/wave/waveform.fst");
+  tfp->open("wave/waveform.fst");
 #endif
 }
 //=====================================================
