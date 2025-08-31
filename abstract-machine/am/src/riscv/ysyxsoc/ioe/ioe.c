06ef9b65e53c67835da9242b28a1e75e3f76a7b0
tracer-ysyx <tracer@ysyx.org>
2025-08-31 12:25:53 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:25:53 up 13:41,  1 user,  load average: 0.82, 0.90, 0.81

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
@@ -4,10 +4,11 @@
 void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
-void __am_uart_config(AM_UART_CONFIG_T *);
 void __am_uart_rx(AM_UART_RX_T *);
-static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 
+static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
+static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
+static void __am_uart_config(AM_UART_CONFIG_T *cfg)   { cfg->present = true; }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
@@ -16,6 +17,7 @@ static void *lut[128] = {
   [AM_TIMER_UPTIME] = __am_timer_uptime,
   [AM_UART_CONFIG ] = __am_uart_config,
   [AM_UART_RX     ] = __am_uart_rx,
+  [AM_INPUT_CONFIG] = __am_input_config,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
