21e1250c6f44d46058035fd55c9d1beb3b301e21
tracer-ysyx <tracer@ysyx.org>
2025-09-01 19:40:09 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:40:09 up  7:46,  1 user,  load average: 1.03, 0.83, 0.76

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/input.c
@@ -1,54 +1,90 @@
 #include <am.h>
 #include <riscv/ysyxsoc/ysyxsoc.h>
 
-const int ps2_scancode_to_amkey[128] = {
+const int ps2_scancode_to_amkey[256] = {
     // 0x00 - 0x0F
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, 
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, 
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_GRAVE, AM_KEY_NONE,
+    0, AM_KEY_F9, 0, AM_KEY_F5, 
+    AM_KEY_F3, AM_KEY_F1, AM_KEY_F2, AM_KEY_F12,
+    0, AM_KEY_F10, AM_KEY_F8, AM_KEY_F6, 
+    AM_KEY_F4, 0, AM_KEY_GRAVE, 0,
     
     // 0x10 - 0x1F
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_Q,    AM_KEY_1,    AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_Z,    AM_KEY_S,
-    AM_KEY_A,    AM_KEY_W,    AM_KEY_2,    AM_KEY_NONE,
+    0, 0, 0, 0,
+    0, AM_KEY_Q,    AM_KEY_1,    0,
+    0, 0, AM_KEY_Z,    AM_KEY_S,
+    AM_KEY_A,    AM_KEY_W,    AM_KEY_2,    0,
     
     // 0x20 - 0x2F
-    AM_KEY_NONE, AM_KEY_C,    AM_KEY_X,    AM_KEY_D,
-    AM_KEY_E,    AM_KEY_4,    AM_KEY_3,    AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_SPACE,AM_KEY_V,    AM_KEY_F,
-    AM_KEY_T,    AM_KEY_R,    AM_KEY_5,    AM_KEY_NONE,
+    0, AM_KEY_C,    AM_KEY_X,    AM_KEY_D,
+    AM_KEY_E,    AM_KEY_4,    AM_KEY_3,    0,
+    0, AM_KEY_SPACE,AM_KEY_V,    AM_KEY_F,
+    AM_KEY_T,    AM_KEY_R,    AM_KEY_5,    0,
     
     // 0x30 - 0x3F
-    AM_KEY_NONE, AM_KEY_N,    AM_KEY_B,    AM_KEY_H,
-    AM_KEY_G,    AM_KEY_Y,    AM_KEY_6,    AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_M,    AM_KEY_J,
-    AM_KEY_U,    AM_KEY_7,    AM_KEY_8,    AM_KEY_NONE,
+    0, AM_KEY_N,    AM_KEY_B,    AM_KEY_H,
+    AM_KEY_G,    AM_KEY_Y,    AM_KEY_6,    0,
+    0, 0, AM_KEY_M,    AM_KEY_J,
+    AM_KEY_U,    AM_KEY_7,    AM_KEY_8,    0,
     
     // 0x40 - 0x4F
-    AM_KEY_NONE, AM_KEY_COMMA,AM_KEY_K,    AM_KEY_I,
-    AM_KEY_O,    AM_KEY_0,    AM_KEY_9,    AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_PERIOD,AM_KEY_SLASH,AM_KEY_L,
-    AM_KEY_SEMICOLON, AM_KEY_P, AM_KEY_MINUS, AM_KEY_NONE,
+    0, AM_KEY_COMMA,AM_KEY_K,    AM_KEY_I,
+    AM_KEY_O,    AM_KEY_0,    AM_KEY_9,    0,
+    0, AM_KEY_PERIOD,AM_KEY_SLASH,AM_KEY_L,
+    AM_KEY_SEMICOLON, AM_KEY_P, AM_KEY_MINUS, 0,
     
     // 0x50 - 0x5F
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_APOSTROPHE, AM_KEY_NONE,
-    AM_KEY_LEFTBRACKET, AM_KEY_EQUALS, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_RETURN, AM_KEY_RIGHTBRACKET,
-    AM_KEY_NONE, AM_KEY_BACKSLASH, AM_KEY_NONE, AM_KEY_NONE,
+    0, 0, AM_KEY_APOSTROPHE, 0,
+    AM_KEY_LEFTBRACKET, AM_KEY_EQUALS, 0, 0,
+    0, 0, AM_KEY_RETURN, AM_KEY_RIGHTBRACKET,
+    0, AM_KEY_BACKSLASH, 0, 0,
     
     // 0x60 - 0x6F
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_BACKSPACE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
+    0, 0, 0, 0,
+    0, 0, AM_KEY_BACKSPACE, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
     
     // 0x70 - 0x7F
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE,
-    AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE, AM_KEY_NONE };
+    0, 0, 0, 0,
+    0, 0, AM_KEY_ESCAPE, 0,
+    AM_KEY_F11, 0, 0, 0,
+    0, 0, 0, 0,
+    // 0x80 - 0x8F
+    0, 0, 0, AM_KEY_F7,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    // 0x90 - 0x9F
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    // 0xa0 - 0xaF
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    // 0xc0 - 0xcF
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    // 0xd0 - 0xdF
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    // 0xe0 - 0xeF
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    // 0xf0 - 0xfF
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+   };
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keycode = inb(PS2_ADDR);
