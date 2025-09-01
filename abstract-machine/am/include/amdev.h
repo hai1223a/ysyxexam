a9af359bb2c2522f642828bf025a9542dfe493da
tracer-ysyx <tracer@ysyx.org>
2025-09-01 15:19:43 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:19:43 up  3:26,  1 user,  load average: 1.06, 0.88, 0.58

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -32,6 +32,8 @@ AM_DEVREG(22, NET_STATUS,   RD, int rx_len, tx_len);
 AM_DEVREG(23, NET_TX,       WR, Area buf);
 AM_DEVREG(24, NET_RX,       WR, Area buf);
 
+// ysyxsoc ps2编码
+
 // Input
 
 #define AM_KEYS(_) \
