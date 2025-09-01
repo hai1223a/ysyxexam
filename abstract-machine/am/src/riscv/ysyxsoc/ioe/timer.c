81f022d540819ad65c73f51f5f01508fe2fcb8aa
tracer-ysyx <tracer@ysyx.org>
2025-09-01 23:22:45 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:22:45 up 11:29,  1 user,  load average: 1.93, 1.41, 1.26

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/timer.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/timer.c
@@ -8,7 +8,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(RTC_ADDR + 4);
   uptime->us <<= 32;
   uptime->us += inl(RTC_ADDR);
-  uptime->us *= 2;
+  uptime->us *= 1;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
