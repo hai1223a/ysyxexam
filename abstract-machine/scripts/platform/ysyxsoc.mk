8f273bf513ec9c46655dfa5a60f8fb7c7c61f690
tracer-ysyx <tracer@ysyx.org>
2025-09-02 22:28:08 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:28:08 up  8:18,  1 user,  load average: 0.29, 0.15, 0.17

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -40,9 +40,6 @@ image: image-dep
 run: insert-arg
 	@$(MAKE) -C $(NPC_HOME) run ARGS="$(NPCFLAGS)"
 
-nvboard: insert-arg
-	@$(MAKE) -C $(NPC_HOME) nvboard ARGS="$(NPCFLAGS)"
-
 gdb: insert-arg
 	@$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)"
 
