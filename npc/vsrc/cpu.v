e564a08c791212ff8b8d815aad12c229c0667dd1
tracer-ysyx <tracer@ysyx.org>
2025-09-03 15:45:35 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:45:35 up 1 day,  1:35,  1 user,  load average: 0.62, 0.86, 0.80

--- npc/vsrc/cpu.v
+++ npc/vsrc/cpu.v
@@ -123,8 +123,8 @@ always @(posedge clk) begin
         if(inst_rvalid_i) if_wait <= 0;
         if(mem_awvalid_o | mem_arvalid_o) lsu_wait <= 1;
         if(mem_bvalid_i | mem_bvalid_i) lsu_wait <= 0;
-        if(if_wait) if_cycle_get();
-        if(lsu_wait) lsu_cycle_get();
+        if(if_wait | inst_arvalid_o) if_cycle_get();
+        if(lsu_wait | mem_awvalid_o | mem_arvalid_o) lsu_cycle_get();
     end
 end
 `endif
