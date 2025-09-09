7d82b2de897a92964ba68b712a779b8ee650f13c
tracer-ysyx <tracer@ysyx.org>
2025-09-10 00:12:16 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:12:16 up  4:57,  1 user,  load average: 0.74, 0.98, 0.96

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,8 +24,8 @@ $(BINARY):: compile_git
 
 # Some convenient rules
 
-override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt --itracebin=$(BUILD_DIR)/itrace.bin
-override ARGS += $(ARGS_DIFF)
+override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt 
+override ARGS += $(ARGS_DIFF) --itracebin=$(BUILD_DIR)/itrace.bin
 
 # Command to execute NEMU
 IMG ?=
