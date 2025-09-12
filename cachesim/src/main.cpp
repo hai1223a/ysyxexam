94d4072a65368ee51a3d7f6574eba0eb47a30052
tracer-ysyx <tracer@ysyx.org>
2025-09-12 20:06:33 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:33 up  4:04,  1 user,  load average: 0.49, 0.74, 0.85

--- cachesim/src/main.cpp
+++ cachesim/src/main.cpp
@@ -1,18 +1,28 @@
 #include "cache.h"
 #include <iostream>
+#include <cassert>
 
-int main( int argc, char* argv[] )
-{
-    CACHE top(2, 4, 2);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cerr << "用法: " << argv[0] << " <压缩文件路径>" << endl;
+        return 1;
+    }
+    string file_path = argv[1];
+    string command = "bzcat " + file_path;
+    CACHE top(2, 5, 4);
     top.printTagSram();
-    top.readCheck(0x1234ffff);
-    top.readCheck(0x1234f32f);
-    top.readCheck(0x1234f00f);
-    top.readCheck(0x1234f12f);
-    top.readCheck(0x1234feef);
-    top.readCheck(0x1234feef);
+    uint32_t buffer[1024];
+    size_t count;
+    FILE *fp = popen(command.c_str(), "r");
+    assert(fp);
+    while((count = fread(buffer, sizeof(uint32_t), 1024, fp)) > 0) {
+        for(size_t i = 0; i < count; i++) {
+            top.readCheck(buffer[i]);
+        }
+    }
+    pclose(fp);
     top.printTagSram();
+    
     cout << top.c_pc << " " << top.c_reach << endl;
     return 0;
 }
-
