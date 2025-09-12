0a6404717fd11127498033f70f23cd8c62acd8a4
tracer-ysyx <tracer@ysyx.org>
2025-09-12 17:24:32 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:24:32 up  1:22,  1 user,  load average: 0.24, 0.69, 0.59

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,7 +13,8 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker_nemu.ld
 LDFLAGS   += --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 override NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt  \
-                      -k $(shell dirname $(IMAGE).elf)/nemu-dtracer-log.txt 
+                      -k $(shell dirname $(IMAGE).elf)/nemu-dtracer-log.txt \
+					  -t $(shell dirname $(IMAGE).elf)/nemu-$(IMAGE)-itrace.bin \
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
 CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=\""$(MAINARGS_PLACEHOLDER)"\"
