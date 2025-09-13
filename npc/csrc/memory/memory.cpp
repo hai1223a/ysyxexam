052afb7dd2ae7bf18f59ddbdb7eaee483c560f05
tracer-ysyx <tracer@ysyx.org>
2025-09-14 00:11:47 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:11:47 up 51 min,  1 user,  load average: 0.54, 0.57, 0.55

--- npc/csrc/memory/memory.cpp
+++ npc/csrc/memory/memory.cpp
@@ -21,7 +21,7 @@ long init_imem(char *img_file)
     *imem_w++ = 0x67830313;
     *imem_w++ = 0x0062a023;
     *imem_w++ = 0x0002a383;
-    *imem_w++ = 0x00028067; // ebreak 
+    *imem_w++ = 0x00100073; // ebreak 
     Log("没有给源文件, 程序使用了内置的代码.");
     return 20;
   }
