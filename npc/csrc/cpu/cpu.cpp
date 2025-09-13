874831177d406d73e4cd3a9d905489475378fddb
tracer-ysyx <tracer@ysyx.org>
2025-09-13 21:55:09 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:09 up 57 min,  1 user,  load average: 0.82, 0.63, 0.47

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -36,7 +36,7 @@ void cpu_exec_once()
       pc_pre = pc__;
       pc__ = SOC_PC;
       if((sim_time >= (reset_time + stop_time)) & pc__ != RESET_VECTOR) {
-        inst_pre = *(uint32_t *)(imem + pc_pre - CONFIG_IMEM_BASE);
+        // inst_pre = *(uint32_t *)(imem + pc_pre - CONFIG_IMEM_BASE);
         inst_count++;
         break;
       }
