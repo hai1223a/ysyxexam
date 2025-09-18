8a8067d8a6bee67635f43ee18f42e7dc8eff36e1
tracer-ysyx <tracer@ysyx.org>
2025-09-18 10:34:11 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:11 up 18:42,  1 user,  load average: 1.62, 1.20, 0.96

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -78,12 +78,12 @@ module ysyx_25050136_LSU
     // 内部
     assign byte_sel = 4'b1 << mem_addr_i[1:0];
 `ifdef VERILATOR_DPIC
+    wire is_mmio = ((mem_addr_i >= 32'h0200_0000) && (mem_addr_i < 32'h0201_0000)) ||
+                   ((mem_addr_i >= 32'h1000_0000) && (mem_addr_i < 32'h1000_1000));
+    wire is_memop = mem_wen_i | mem_ren_i;
     always @(*) begin
-        if ((((mem_addr_i >= 32'h0200_0000) && (mem_addr_i < 32'h0201_0000)) ||
-             ((mem_addr_i >= 32'h1000_0000) && (mem_addr_i < 32'h1000_1000))) && (mem_wen_i | mem_ren_i))
-        begin
+        if (is_mmio && is_memop) begin
             find_diff_skip();
-            
         end
     end
 `endif
