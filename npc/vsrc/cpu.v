30490c7c697a19b4cb829dc3ad1f34e4ea79f3c8
tracer-ysyx <tracer@ysyx.org>
2025-09-20 13:38:11 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:38:11 up  1:53,  1 user,  load average: 0.75, 0.98, 0.93

--- npc/vsrc/cpu.v
+++ npc/vsrc/cpu.v
@@ -67,11 +67,11 @@ module ysyx_25050136_NPC
         if(inst_req_valid & inst_req_ready) ifu_get();
         if(inst_req_valid & !inst_req_ready) ifu_cycle();
         if(inst_req_valid & inst_req_ready & inst_req_use) icache_get();
-        // if(inst_req_valid & !inst_req_ready & inst_req_use) icache_cycle();
+        if(inst_req_valid & !inst_req_ready & inst_req_use) icache_cycle();
         if(mem_req_valid & mem_req_ready) lsu_noclint_get();
-        // if(mem_req_valid & !mem_req_ready) lsu_noclint_cycle();
+        if(mem_req_valid & !mem_req_ready) lsu_noclint_cycle();
         if(mem_req_valid & mem_req_ready & mem_req_use) dcache_get();
-        // if(mem_req_valid & !mem_req_ready & mem_req_use) dcache_cycle();
+        if(mem_req_valid & !mem_req_ready & mem_req_use) dcache_cycle();
     end
 `endif
     wire [31:0] inst_req_addr;
