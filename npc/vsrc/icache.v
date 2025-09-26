861b66b2a8360d5d2b0c58ddd635c0f6e3e176f4
tracer-ysyx <tracer@ysyx.org>
2025-09-27 00:17:09 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:17:09 up  8:16,  1 user,  load average: 0.50, 0.49, 0.44

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -4,7 +4,7 @@ module ysyx_25050136_ICACHE
 #(
     parameter OFFSET_WIDTH = 3,
     parameter NUM_WAY = 2,
-    parameter INDEX_WIDTH = 4
+    parameter INDEX_WIDTH = 2
 )
 (
     input                                      clk          ,
