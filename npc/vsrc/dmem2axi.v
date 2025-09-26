d34ae9bed473c37e4018011d61be12eea850812c
tracer-ysyx <tracer@ysyx.org>
2025-09-26 14:49:30 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:30 up  1:24,  1 user,  load average: 0.89, 0.89, 0.71

---
+++ npc/vsrc/dmem2axi.v
@@ -0,0 +1,359 @@
+module ysyx_25050136_DMEM2AXI
+(
+    input                                      clk          ,
+    input                                      reset        ,
+    // 读地址                                        
+    output                                     m_arvalid_o  ,
+    input                                      m_arready_i  ,
+    output   [31:0]                            m_araddr_o   ,
+    output   [3:0]                             m_arid_o     ,
+    output   [7:0]                             m_arlen_o    ,
+    output   [2:0]                             m_arsize_o   ,
+    output   [1:0]                             m_arburst_o  ,
+    // 读数据                                        
+    input                                      m_rvalid_i   ,
+    output                                     m_rready_o   ,
+    input    [31:0]                            m_rdata_i    ,
+    input    [1:0]                             m_rresp_i    ,
+    input                                      m_rlast_i    ,
+    input    [3:0]                             m_rid_i      ,
+    // 内部
+    input    [31:0]                            req_addr_i   ,
+    input                                      req_valid_i  ,
+    input                                      req_use_i    ,
+    output   [31:0]                            req_rdata_o  ,                           
+    output                                     req_ready_o  
+);
+    // ====================cache内部信号定义==============================
+    parameter LINE_WIDTH = 8 * 2 ** OFFSET_WIDTH;
+    parameter TAG_WIDTH = 32 - OFFSET_WIDTH - INDEX_WIDTH;
+    parameter NUM_SET = 2 ** INDEX_WIDTH;
+    parameter WAY_WIDTH = NUM_WAY>1?$clog2(NUM_WAY):1;
+    parameter BURST_NUM = 2 ** (OFFSET_WIDTH - 2) - 1;
+    // icache状态机
+    localparam IDLE      = 2'd0;
+    localparam INCACHE   = 2'd1;
+    localparam CACHEMISS = 2'd2;
+    localparam MISSIN    = 2'd3;
+    // icache存储阵列
+    reg [LINE_WIDTH-1:0] cache_data  [0:NUM_WAY-1][0:NUM_SET-1];
+    reg [TAG_WIDTH-1 :0] cache_tag   [0:NUM_WAY-1][0:NUM_SET-1];
+    reg                  cache_valid [0:NUM_WAY-1][0:NUM_SET-1];
+    reg [LINE_WIDTH-1:0] cache_data_temp;
+    // icache与CPU交互信号
+    reg [1:0] state;
+    reg [31:0] req_addr_r;
+    reg [31:0] req_rdata_r;
+    reg req_ready_r;
+    // 地址解析
+    wire [INDEX_WIDTH-1:0] addr_index = req_addr_r[OFFSET_WIDTH+INDEX_WIDTH-1:OFFSET_WIDTH];
+    wire [TAG_WIDTH-1:0] addr_tag = req_addr_r[31:OFFSET_WIDTH+INDEX_WIDTH];
+    wire [OFFSET_WIDTH-1:0] addr_offset = req_addr_r[OFFSET_WIDTH-1:0];
+    // 命中信号
+    wire [NUM_WAY-1:0] way_hit;
+    wire [NUM_WAY-1:0] way_valid;
+    wire [NUM_WAY-1:0] hot_hit = way_hit & way_valid;
+    wire [WAY_WIDTH-1:0] bin_hit;
+    generate
+        for (genvar i = 0; i < NUM_WAY; i = i + 1) begin
+            assign way_valid[i] = cache_valid[i][addr_index];
+            assign way_hit[i] = (cache_tag[i][addr_index] == addr_tag);
+        end
+    endgenerate
+    ysyx_25050136_hot2bin #(.ONE_HOT_WIDTH(NUM_WAY))
+        hot2bin_hit (
+            .one_hot_code(hot_hit),
+            .bin_code(bin_hit)
+        );
+    // 替换索引生成逻辑(随机替换) 
+    reg [WAY_WIDTH-1:0] replace_way;
+    reg [WAY_WIDTH-1:0] replace_way_use;
+    wire [WAY_WIDTH-1:0] replace_way_dirty;
+    wire replace_way_dirty_valid;
+    always @(posedge clk) begin
+        if (reset) begin
+            replace_way <= 0;
+        end else begin
+            if(NUM_WAY > 1) begin
+                replace_way <= replace_way + 1;
+            end
+        end
+    end
+    ysyx_25050136_encoder #(.WIDTH(NUM_WAY))
+        replace_encoder (
+            .in_code(~way_valid),
+            .out_code(replace_way_dirty),
+            .valid(replace_way_dirty_valid)
+        );
+    // axi读请求计数
+    reg [7:0] axi_read_cnt;
+    // axi状态机判断信号
+    reg axi_start1; // ifu取指直接使用axi
+    reg axi_start2; // ifu取指在cache未命中使用axi
+
+
+    // ====================axi信号定义================================
+    localparam READ_IDLE = 2'd0;
+    localparam READ_ADDR = 2'd1;
+    localparam READ_DATA = 2'd2;
+    // axi读请求信号
+    reg [1:0] state_read;
+    reg m_rready_r;
+    reg [3:0] m_arid_r;
+    reg [31:0] m_araddr_r;
+    reg [7:0] m_arlen_r;
+    reg [2:0] m_arsize_r;
+    reg [1:0] m_arburst_r;
+    wire ar_fire, r_fire;
+    // ====================icache逻辑实现==============================
+    // icache总状态机
+    integer i,j;
+    always @(posedge clk) begin
+        if (reset) begin
+            state <= READ_IDLE;
+            req_addr_r <= 0;
+            for (i = 0; i < NUM_WAY; i = i + 1) begin
+                for (j = 0; j < NUM_SET; j = j + 1) begin
+                    cache_valid[i][j] <= 0;
+                end
+            end
+        end else begin
+            case (state)
+            IDLE: begin
+                if(req_valid_i) begin
+                    if (req_use_i) begin
+                        state <= INCACHE;
+                    end else begin
+                        state <= CACHEMISS;                            
+                    end
+                    req_addr_r <= req_addr_i;
+                end         
+            end 
+            INCACHE: begin
+                if(hot_hit != 0) begin
+`ifdef ysyx_25050136_VERILATOR_DPIC
+                    icache_hit();
+`endif
+                    state <= IDLE;
+                end else begin
+                    state <= CACHEMISS;
+                end
+            end
+            CACHEMISS: begin
+                if(r_fire && m_rlast_i && (m_rresp_i == 2'd0)) begin
+                    state <= MISSIN;
+                end
+            end
+            MISSIN: begin
+                if(req_use_i) begin
+                    cache_data[replace_way_use][addr_index] <= cache_data_temp;
+                    cache_tag[replace_way_use][addr_index] <= addr_tag;
+                    cache_valid[replace_way_use][addr_index] <= 1'b1;
+                end
+                state <= IDLE;
+            end
+            endcase
+        end
+    end
+    
+    always @(*) begin
+        req_rdata_r = 0;
+        req_ready_r = 0;
+        axi_start1 = 0;
+        axi_start2 = 0;
+        case (state)
+            IDLE: begin
+                if(req_valid_i & ~req_use_i) begin
+                    axi_start1 = 1;
+                end
+            end
+            INCACHE: begin
+            if(hot_hit != 0) begin
+                req_rdata_r = cache_data[bin_hit][addr_index][addr_offset*8 +: 32];
+                req_ready_r = 1;               
+            end else begin
+                axi_start2 = 1;
+            end
+            end 
+            MISSIN: begin
+                if(req_use_i) begin
+                    req_rdata_r = cache_data_temp[addr_offset*8 +: 32];
+                end else begin
+                    req_rdata_r = cache_data_temp[31:0];
+                end
+                req_ready_r = 1;               
+            end
+            default: ;
+        endcase
+    end
+
+    // reg [31:0] req_rdata_r2;
+    // reg req_ready_r2;
+    // always@(posedge clk) begin
+    //     if(reset) begin
+    //         req_addr_r2 <= 0;
+    //         req_ready_r2 <= 0;
+    //     end else begin
+    //         if(req_ready_r) begin
+    //             req_rdata_r <= req_rdata_r;
+    //             req_ready_r <= req_ready_r;                
+    //         end
+    //     end
+    // end
+    assign req_rdata_o = req_rdata_r;
+    assign req_ready_o = req_ready_r;
+
+    // axi读请求状态机
+    always @(posedge clk) begin
+        if (reset) begin
+            state_read      <= READ_IDLE;
+            cache_data_temp <= 0;
+            replace_way_use <= 0;
+            axi_read_cnt    <= 0;
+            m_araddr_r      <= 0;
+            m_arid_r        <= 0;
+            m_arlen_r       <= 0;   
+            m_arsize_r      <= 0;
+            m_arburst_r     <= 0;
+            m_rready_r      <= 0;
+        end else begin
+            case(state_read)
+            READ_IDLE: begin
+                m_rready_r <= 1;
+                axi_read_cnt <= 0;
+                if (axi_start1 | axi_start2) begin
+                    state_read <= READ_ADDR;
+                    // 默认赋值（单次读）
+                    m_araddr_r  <= req_addr_i;
+                    m_arid_r    <= 4'b1001;
+                    m_arlen_r   <= 0;
+                    m_arsize_r  <= 3'b010;
+                    m_arburst_r <= 2'b00;
+                    // 若为cache miss触发的burst读，覆盖相关参数
+                    if (axi_start2) begin
+                        m_araddr_r  <= {req_addr_r[31:OFFSET_WIDTH], {OFFSET_WIDTH{1'b0}}};
+                        m_arlen_r   <= BURST_NUM;      // burst长度
+                        m_arburst_r <= 2'b01;          // 增量式
+                    end
+                end
+            end 
+            READ_ADDR: begin
+                m_rready_r <= 1;
+                if (ar_fire) begin
+                    replace_way_use <= replace_way_dirty_valid ? replace_way_dirty : replace_way;
+                    m_araddr_r <= 0;
+                    m_arlen_r <= 0;
+                    m_arsize_r <= 0;
+                    m_arburst_r <= 0;
+                    state_read <= READ_DATA;                        
+                end
+            end 
+            READ_DATA: begin
+                if (r_fire) begin
+                    axi_read_cnt <= axi_read_cnt + 1;
+                    cache_data_temp[32*axi_read_cnt +: 32] <= m_rdata_i;
+                    if(m_rlast_i) begin
+                        m_arid_r <= 0;
+                        state_read <= READ_IDLE;
+                    end
+                    m_rready_r <= 0;
+                end else begin
+                    m_rready_r <= 1;                        
+                end
+            end 
+            default: ;
+            endcase
+        end
+    end
+
+    assign m_arvalid_o = (state_read == READ_ADDR);
+    assign m_araddr_o  = m_araddr_r;
+    assign m_arid_o = m_arid_r;
+    assign m_arlen_o = m_arlen_r;
+    assign m_arsize_o = m_arsize_r;
+    assign m_arburst_o = m_arburst_r;
+    assign m_rready_o = m_rready_r;
+    assign ar_fire = m_arvalid_o & m_arready_i;
+    assign r_fire = m_rvalid_i & m_rready_o;
+
+// ========================Simulation only====================================
+`ifdef verilator
+reg [79:0] dbg_cache_state;
+reg [79:0] dbg_axi_state;
+always @(*) begin
+    case (state)
+        IDLE        : dbg_cache_state = "IDLE";
+        INCACHE     : dbg_cache_state = "INCACHE";
+        CACHEMISS   : dbg_cache_state = "CACHEMISS";
+        MISSIN      : dbg_cache_state = "MISSIN";
+        default     : dbg_cache_state = "UNKNOW";
+    endcase
+    case (state_read)
+        READ_IDLE   : dbg_axi_state = "READ_IDLE";
+        READ_ADDR   : dbg_axi_state = "READ_ADDR";
+        READ_DATA   : dbg_axi_state = "READ_DATA";
+        default     : dbg_axi_state = "UNKNOW";
+    endcase
+end
+`endif
+endmodule //ysyx_25050136_ICACHE
+
+module ysyx_25050136_hot2bin
+#(
+    parameter  ONE_HOT_WIDTH = 4,
+    parameter  PRIVATE_TEMP0 = $clog2(ONE_HOT_WIDTH),
+    parameter  PRIVATE_TEMP1 = (ONE_HOT_WIDTH>1?PRIVATE_TEMP0:1)    
+)
+(
+    input   [ONE_HOT_WIDTH-1 : 0]       one_hot_code,
+    output  [PRIVATE_TEMP1-1 : 0]       bin_code
+);
+
+    wire [PRIVATE_TEMP1-1 : 0] temp1 [ONE_HOT_WIDTH-1 : 0];
+	wire [ONE_HOT_WIDTH-1 : 0] temp2 [PRIVATE_TEMP1-1 : 0];
+		
+	genvar i,j,k;
+	generate
+		for(i = 0; i < ONE_HOT_WIDTH; i = i+1)begin : temp1_loop
+			assign temp1[i] = one_hot_code[i]? i[PRIVATE_TEMP1-1:0] : 'b0;
+		end
+	endgenerate
+	generate
+		for(i = 0; i < ONE_HOT_WIDTH; i = i+1)begin : temp_ch1
+			for(j = 0; j < PRIVATE_TEMP1; j = j+1)begin  : temp_ch2
+				assign temp2[j][i] = temp1[i][j];
+			end
+		end
+	endgenerate
+	generate
+		for(j = 0; j < PRIVATE_TEMP1; j = j+1)begin : temp2_loop
+			assign bin_code[j] = |temp2[j];
+		end
+	endgenerate
+    
+endmodule //hot2bin
+
+module ysyx_25050136_encoder
+#(
+    parameter WIDTH = 4,
+    parameter PRIVATE_TEMP0 = $clog2(WIDTH),
+    parameter PRIVATE_TEMP1 = (WIDTH>1?PRIVATE_TEMP0:1)  
+)
+(
+    input   [WIDTH-1 : 0]             in_code,
+    output  reg [PRIVATE_TEMP1-1 : 0] out_code,
+    output  reg                       valid
+);
+    integer i;
+    always @(*) begin
+        out_code = 'b0;
+        valid = 'b0;
+        for(i = 0; i < WIDTH; i = i+1) begin
+            if(in_code[i]) begin
+                out_code = i[PRIVATE_TEMP1-1:0];
+                valid = 'b1;
+            end
+        end
+    end
+    
+endmodule //hot2bin
---
+++ npc/vsrc/imem2axi.v
@@ -0,0 +1,129 @@
+module ysyx_25050136_IMEM2AXI
+#(
+    parameter OFFSET_WIDTH = 4 // cache line = 16B
+)
+(
+    input                                      clk          ,
+    input                                      reset        ,
+    // 读地址                                        
+    output                                     m_arvalid_o  ,
+    input                                      m_arready_i  ,
+    output   [31:0]                            m_araddr_o   ,
+    output   [3:0]                             m_arid_o     ,
+    output   [7:0]                             m_arlen_o    ,
+    output   [2:0]                             m_arsize_o   ,
+    output   [1:0]                             m_arburst_o  ,
+    // 读数据                                        
+    input                                      m_rvalid_i   ,
+    output                                     m_rready_o   ,
+    input    [31:0]                            m_rdata_i    ,
+    input    [1:0]                             m_rresp_i    ,
+    input                                      m_rlast_i    ,
+    input    [3:0]                             m_rid_i      ,
+    // 内部
+    input                                      rd_req_i     ,
+    input                                      rd_size_i    , // 0: word 1: cahce line                
+    input    [31:0]                            rd_addr_i    ,
+    output                                     rd_rdy_o     ,
+    output                                     ret_valid_o  ,
+    output                                     ret_last_o   ,
+    output   [31:0]                            ret_data_o   
+);    
+    // ==================== axi信号定义 ================================
+    parameter BURST_NUM = 2 ** (OFFSET_WIDTH - 2) - 1;
+    localparam READ_IDLE = 2'd0;
+    localparam READ_ADDR = 2'd1;
+    localparam READ_DATA = 2'd2;
+    // axi读请求信号
+    reg [1:0] state_read;
+    reg m_rready_r;
+    reg [3:0] m_arid_r;
+    reg [31:0] m_araddr_r;
+    reg [7:0] m_arlen_r;
+    reg [2:0] m_arsize_r;
+    reg [1:0] m_arburst_r;
+    wire ar_fire, r_fire;
+    // ==================== axi信号定义 ================================
+    // 读事务
+    always @(posedge clk) begin
+        if (reset) begin
+            state_read      <= READ_IDLE;
+            m_araddr_r      <= 0;
+            m_arid_r        <= 0;
+            m_arlen_r       <= 0;   
+            m_arsize_r      <= 0;
+            m_arburst_r     <= 0;
+            m_rready_r      <= 0;
+        end else begin
+            case(state_read)
+                READ_IDLE: begin
+                    m_rready_r <= 1;
+                    if (rd_req_i) begin
+                        state_read <= READ_ADDR;
+                        m_arid_r    <= 4'b1001;
+                        m_arsize_r  <= 3'b010;
+                        m_arburst_r <= 2'b00;
+                        if(rd_size_i) begin // cache line
+                            m_araddr_r  <= {req_addr_r[31:OFFSET_WIDTH], {OFFSET_WIDTH{1'b0}}};
+                            m_arlen_r   <= BURST_NUM;      
+                            m_arburst_r <= 2'b01;          
+                        end else begin // word
+                            m_araddr_r  <= rd_addr_i;
+                            m_arlen_r   <= 0;
+                            m_arburst_r <= 2'b00;
+                        end
+                    end
+                end 
+                READ_ADDR: begin
+                    if (ar_fire) begin
+                        m_araddr_r <= 0;
+                        m_arlen_r <= 0;
+                        m_arsize_r <= 0;
+                        m_arburst_r <= 0;
+                        state_read <= READ_DATA;                        
+                    end
+                end 
+                READ_DATA: begin
+                    if (r_fire) begin
+                        if(m_rlast_i) begin
+                            m_arid_r <= 0;
+                            state_read <= READ_IDLE;
+                        end
+                        m_rready_r <= 0;
+                    end else begin
+                        m_rready_r <= 1;                        
+                    end
+                end 
+                default: ;
+            endcase
+        end
+    end
+
+    assign m_arvalid_o = (state_read == READ_ADDR);
+    assign m_araddr_o  = m_araddr_r;
+    assign m_arid_o = m_arid_r;
+    assign m_arlen_o = m_arlen_r;
+    assign m_arsize_o = m_arsize_r;
+    assign m_arburst_o = m_arburst_r;
+    assign m_rready_o = m_rready_r;
+    
+    assign ar_fire = m_arvalid_o & m_arready_i;
+    assign r_fire = m_rvalid_i & m_rready_o;
+
+    assign rd_rdy_o = ar_fire;
+    assign ret_valid_o = r_fire;
+    assign ret_last_o = m_rlast_i & r_fire;
+    assign ret_data_o = m_rdata_i;
+// ========================Simulation only====================================
+`ifdef verilator
+reg [79:0] dbg_axi_state;
+always @(*) begin
+    case (state_read)
+        READ_IDLE   : dbg_axi_state = "READ_IDLE";
+        READ_ADDR   : dbg_axi_state = "READ_ADDR";
+        READ_DATA   : dbg_axi_state = "READ_DATA";
+        default     : dbg_axi_state = "UNKNOW";
+    endcase
+end
+`endif
+endmodule //ysyx_25050136_ICACHE
