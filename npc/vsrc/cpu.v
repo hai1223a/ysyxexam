22d3f8766961816befea1f59fa96f63660e64c0e
tracer-ysyx <tracer@ysyx.org>
2025-09-26 21:35:38 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:35:38 up  5:34,  1 user,  load average: 0.58, 0.57, 0.45

--- npc/vsrc/cpu.v
+++ npc/vsrc/cpu.v
@@ -86,11 +86,6 @@ module ysyx_25050136_NPC
     wire mem_req_use;
     wire [31:0] mem_req_wdata;
     ysyx_25050136_ICACHE_WRAPPER 
-    #(
-        .OFFSET_WIDTH 	(4),
-        .INDEX_WIDTH  	(4),
-        .NUM_WAY     	(1)
-    )
     u_ysyx_25050136_ICACHE(
         .clk         	(clk             ),
         .reset       	(reset           ),
