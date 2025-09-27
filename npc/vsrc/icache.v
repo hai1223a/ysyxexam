b42b2784fb609f9d05c6296870854683a969ecdf
tracer-ysyx <tracer@ysyx.org>
2025-09-28 00:26:15 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:26:15 up  9:34,  1 user,  load average: 0.70, 0.54, 0.41

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -51,7 +51,7 @@ module ysyx_25050136_ICACHE
     // IN_CAHCE & AXI
     reg [LINE_WIDTH-1:0] line_buf ;
     // IN_CACHE
-    reg [OFFSET_WIDTH-1:0] addr_offset_r;
+    reg [OFFSET_WIDTH-3:0] addr_offset_r;
     reg cache_hit;
     reg [LINE_WIDTH-1:0] selected_line;
     reg [NUM_WAY-1:0] hit_mask;
@@ -87,7 +87,7 @@ module ysyx_25050136_ICACHE
                 end
                 IN_CAHCE: begin
                     line_buf <= selected_line;
-                    addr_offset_r <= addr_offset;
+                    addr_offset_r <= addr_offset[OFFSET_WIDTH-1:2];
                     cache_hit <= |hit_mask;
                     if(|hit_mask) begin
                         state <= HIT;
