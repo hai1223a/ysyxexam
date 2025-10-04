1001f4daed0c093911e2c778f95b1376490530b2
tracer-ysyx <tracer@ysyx.org>
2025-10-04 20:21:48 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:21:48 up 21:32,  1 user,  load average: 1.53, 0.68, 0.63

--- npc/vsrc/id.v
+++ npc/vsrc/id.v
@@ -212,7 +212,7 @@ module ysyx_25050136_ID
         end else begin
             if(!stall_i) begin
                 dbg_op_o[`ysyx_25050136_DBG_EBREAK] <= inst_ebreak;
-                dbg_op_o[`ysyx_25050136_DBG_ALU] <= type_op_imm | type_auipc | type_lui | type_op |;
+                dbg_op_o[`ysyx_25050136_DBG_ALU] <= type_op_imm | type_auipc | type_lui | type_op;
                 dbg_op_o[`ysyx_25050136_DBG_CSRU] <= type_system;
                 dbg_op_o[`ysyx_25050136_DBG_LSU] <= type_store | type_load;
                 dbg_op_o[`ysyx_25050136_DBG_BQU] <= type_branch | type_jalr | type_jal;
