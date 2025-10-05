129c00f92f5336ae2f672af6ead49ef28cf62314
tracer-ysyx <tracer@ysyx.org>
2025-10-05 11:21:46 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:46 up 1 day, 12:32,  1 user,  load average: 0.35, 0.17, 0.06

--- npc/vsrc/rom.v
+++ npc/vsrc/rom.v
@@ -10,16 +10,16 @@ module ROM_TEST (
 
     reg [31:0] rom_mem [0:15];
     initial begin
-        rom_mem[0 ] = 32'h00000013;
-        rom_mem[1 ] = 32'h00000013;
-        rom_mem[2 ] = 32'h00000013;
-        rom_mem[3 ] = 32'h00000013;
-        rom_mem[4 ] = 32'h00000013;
-        rom_mem[5 ] = 32'h00000013;
-        rom_mem[6 ] = 32'h00000013;
-        rom_mem[7 ] = 32'h00000013;
-        rom_mem[8 ] = 32'h00000013;
-        rom_mem[9 ] = 32'h00000013;
+        rom_mem[0 ] = 32'h00150513;
+        rom_mem[1 ] = 32'h00150513;
+        rom_mem[2 ] = 32'h00150513;
+        rom_mem[3 ] = 32'h00150513;
+        rom_mem[4 ] = 32'h00150513;
+        rom_mem[5 ] = 32'h00150513;
+        rom_mem[6 ] = 32'h00150513;
+        rom_mem[7 ] = 32'h00150513;
+        rom_mem[8 ] = 32'h00150513;
+        rom_mem[9 ] = 32'h00150513;
         rom_mem[10] = 32'h00100073;
         rom_mem[11] = 32'h00000013;
         rom_mem[12] = 32'h00000013;
