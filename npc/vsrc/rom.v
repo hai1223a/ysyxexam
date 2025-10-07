d16d2ca646cadae85be900ff1ab63f5aeb0bb0d3
tracer-ysyx <tracer@ysyx.org>
2025-10-07 17:33:00 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:33:00 up  3:59,  1 user,  load average: 0.30, 0.29, 0.33

--- npc/vsrc/rom.v
+++ npc/vsrc/rom.v
@@ -48,7 +48,7 @@ module ROM_TEST (
                 1: begin // 输出数据
                     req_rdata_o <= rom_mem[addr];
                     req_ready_o <= 1;
-                    if(req_valid_i) begin
+                    if(req_valid_i & req_ready_o) begin
                         state <= 0;
                     end
                 end
