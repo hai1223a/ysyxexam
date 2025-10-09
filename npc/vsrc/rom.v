84725fc79e26921e2b02a60fe4ddb716af449740
tracer-ysyx <tracer@ysyx.org>
2025-10-09 12:36:32 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:36:32 up 16:39,  1 user,  load average: 0.43, 0.46, 0.29

--- npc/vsrc/rom.v
+++ npc/vsrc/rom.v
@@ -11,16 +11,16 @@ module ROM_TEST (
     reg [31:0] rom_mem [0:15];
     initial begin
         rom_mem[0 ] = 32'h0f000537; // lui   a0,0xf000
-        rom_mem[1 ] = 32'h00500593; // li    a1,5
+        rom_mem[1 ] = 32'h00200593; // li    a1,2
         rom_mem[2 ] = 32'h00b52023; // sw    a1,0(a0)
         rom_mem[3 ] = 32'h00052283; // lw    t0,0(a0)
         rom_mem[4 ] = 32'h00b28333; // add   t1,t0,a1
-        rom_mem[5 ] = 32'h00030463; // beqz  t1,skip
-        rom_mem[6 ] = 32'h00140413; // addi  s0,s0,1
-        rom_mem[7 ] = 32'h40b304b3; // sub   s1,t1,a1
-        rom_mem[8 ] = 32'h00100073; // ebreak
-        rom_mem[9 ] = 32'h00000013;
-        rom_mem[10] = 32'h00000013;
+        rom_mem[5 ] = 32'h00138393; // addi  t2,t2,1
+        rom_mem[6 ] = 32'h40730333; // sub   t1,t1,t2
+        rom_mem[7 ] = 32'hfe031ee3; // bnez  t1,skip
+        rom_mem[8 ] = 32'h00140413; // addi  s0,s0,1
+        rom_mem[9 ] = 32'h00108093; // addi  ra,ra,1
+        rom_mem[10] = 32'h00100073; // ebreak
         rom_mem[11] = 32'h00000013;
         rom_mem[12] = 32'h00000013;
         rom_mem[13] = 32'h00000013;
