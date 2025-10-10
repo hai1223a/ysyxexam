5e5ef940941c7fd8c5dfbd745bb8a956e493579c
tracer-ysyx <tracer@ysyx.org>
2025-10-10 21:52:23 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:23 up  9:22,  1 user,  load average: 0.55, 0.51, 0.42

--- npc/vsrc/soc_only/npc.v
+++ npc/vsrc/soc_only/npc.v
@@ -54,7 +54,7 @@ module NPCCORE_TEST (
             req_use_r   <= 0;
             req_valid_r <= 0;
         end else begin
-            if(!stall & mem_req_ready_i) begin
+            if(!stall & inst_req_ready_i) begin
                 pc <= next_pc;
                 req_use_r <= (next_pc >= 32'ha000_0000) && (next_pc < 32'ha400_0000);   
             end
