86947e7f4c11fc05f633aa4ef9392646ea52142b
tracer-ysyx <tracer@ysyx.org>
2025-10-10 21:13:29 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:29 up  8:43,  1 user,  load average: 0.25, 0.23, 0.17

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -81,7 +81,7 @@ module ysyx_25050136_ICACHE
                     if(req_valid_i) begin
                         line_buf <= selected_line;
                         addr_offset_r <= addr_offset[OFFSET_WIDTH-1:2];
-                        cache_hit <= |hit_mask;
+                        cache_hit <= 1;
                         req_use_r <= req_use_i;
                         req_raddr_r <= req_addr_i;
                     if(~cache_hit) begin state <= MISS;end
