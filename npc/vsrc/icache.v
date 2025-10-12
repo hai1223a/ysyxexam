eb17b0ae50089c66f9c85ada50b9c82d1e4e1593
tracer-ysyx <tracer@ysyx.org>
2025-10-12 19:45:46 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:45:46 up  1:45,  1 user,  load average: 0.68, 0.51, 0.37

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -145,7 +145,6 @@ module ysyx_25050136_ICACHE
     assign hit_word = line_word[addr_offset_2];
     // MISS时阻塞逻辑
     // 命中冲突
-    assign conflict = addr_1[31:OFFSET_WIDTH] == addr_2[31:OFFSET_WIDTH];
     assign miss = ~(|temp_hit_mask);
     always @(posedge clk) begin
         if(reset) begin
