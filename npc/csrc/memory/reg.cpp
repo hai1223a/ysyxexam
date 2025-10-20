f2481ee56699cbf74a1029f9df1afdc94202fd3f
tracer-ysyx <tracer@ysyx.org>
2025-10-20 23:58:23 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:58:23 up  2:38,  1 user,  load average: 0.50, 0.88, 0.86

--- npc/csrc/memory/reg.cpp
+++ npc/csrc/memory/reg.cpp
@@ -17,7 +17,7 @@ void printf_regs()
   {
     printf("%s : 0x%8x\n", regs[i], get_reg(i));
   }
-  printf("pc : 0x%8x\n", SOC_PC);
+  printf("pc : 0x%8x\n", SOC_NPC);
 }
 
 uint32_t reg_str2val(const char *s, bool *success)
@@ -34,7 +34,7 @@ uint32_t reg_str2val(const char *s, bool *success)
   }
   const char *reg = "$pc";
   if (!strcmp(s, reg))
-    return SOC_PC;
+    return SOC_NPC;
   *success = false;
   return 0;
 }
@@ -46,7 +46,7 @@ bool isa_difftest_checkregs(CPU_state *ref)
     if (ref->gpr[i] != get_reg(i))
       return false;
   }
-  if (ref->pc != SOC_PC)
+  if (ref->pc != SOC_NPC)
     return false;
   return true;
 }
