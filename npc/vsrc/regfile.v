5d4cd57ac2485410e0e579097a6fb1c60b5260e4
tracer-ysyx <tracer@ysyx.org>
2025-10-20 23:18:02 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:18:02 up  1:57,  1 user,  load average: 0.27, 0.28, 0.26

--- npc/vsrc/regfile.v
+++ npc/vsrc/regfile.v
@@ -23,4 +23,22 @@ module ysyx_25050136_RegisterFile
 
     assign rdata1_o = (raddr1_i == 0) ? 0 :gpr[raddr1_i];
     assign rdata2_o = (raddr2_i == 0) ? 0 :gpr[raddr2_i];
+`ifdef ysyx_25050136_VERILATOR_DPIC
+    wire [31:0] rf_zero = 32'd0;          // x0
+    wire [31:0] rf_ra   = gpr[1];         // x1
+    wire [31:0] rf_sp   = gpr[2];         // x2
+    wire [31:0] rf_gp   = gpr[3];         // x3
+    wire [31:0] rf_tp   = gpr[4];         // x4
+    wire [31:0] rf_t0   = gpr[5];         // x5
+    wire [31:0] rf_t1   = gpr[6];         // x6
+    wire [31:0] rf_t2   = gpr[7];         // x7
+    wire [31:0] rf_s0   = gpr[8];         // x8 (fp)
+    wire [31:0] rf_s1   = gpr[9];         // x9
+    wire [31:0] rf_a0   = gpr[10];        // x10
+    wire [31:0] rf_a1   = gpr[11];        // x11
+    wire [31:0] rf_a2   = gpr[12];        // x12
+    wire [31:0] rf_a3   = gpr[13];        // x13
+    wire [31:0] rf_a4   = gpr[14];        // x14
+    wire [31:0] rf_a5   = gpr[15];        // x15
+`endif
 endmodule
