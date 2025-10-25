e35bf2ba93d2f4bb18f58a14a4f36fac4a192d62
tracer-ysyx <tracer@ysyx.org>
2025-10-25 22:49:54 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:49:54 up 2 days,  9:54,  1 user,  load average: 1.04, 1.06, 1.28

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -36,6 +36,7 @@ void cpu_exec_once()
     if (SOC_PC != pc__ && SOC_PC != 0 && SOC_NPC != SOC_PC)
     {
         printf("检查:SOC_PC: %x SOC_INST: %x pc__: %x inst__: %x\n",SOC_PC, SOC_INST, pc__,inst__);
+        printf("is skip = %x", if_skip);
             pc__ = SOC_PC;
             inst__ = SOC_INST;
             inst_count++;
