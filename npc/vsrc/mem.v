2c680a0bf9c8667d77ea4f13b18b96b735fed216
tracer-ysyx <tracer@ysyx.org>
2025-10-25 23:12:35 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:12:35 up 2 days, 10:16,  1 user,  load average: 1.16, 1.37, 1.27

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -193,9 +193,21 @@ module ysyx_25050136_MEM
     assign in_ready_o = idle || out_fire;
     assign out_valid_o = !(idle || flush) && ready_go;
 `ifdef ysyx_25050136_VERILATOR_DPIC
-    assign out_dbg_is_device_o = !((req_addr_o >= 32'h3000_0000 && req_addr_o < 32'h4000_0000) || 
-                                (req_addr_o >= 32'h0f00_0000 && req_addr_o < 32'h1000_0000) ||
-                                (req_addr_o >= 32'ha000_0000 && req_addr_o < 32'hc000_0000));
+    assign out_dbg_is_device_o = !();
+    always @(posedge clk) begin
+        if(reset) begin
+            out_dbg_is_device_o <= 0;
+        end else begin
+            if(req_valid_o)begin
+                if(!((req_addr_o >= 32'h3000_0000 && req_addr_o < 32'h4000_0000) || 
+                   (req_addr_o >= 32'h0f00_0000 && req_addr_o < 32'h1000_0000) ||
+                   (req_addr_o >= 32'ha000_0000 && req_addr_o < 32'hc000_0000)))
+                    out_dbg_is_device_o <= 1;
+            end else if(out_fire) begin
+                    out_dbg_is_device_o <= 0;                
+            end
+        end
+    end
     always @(posedge clk) begin
         if(reset) begin
             out_dbg_pc_o   <= 0;
