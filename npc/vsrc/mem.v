a692ad83bb62ed89354ac9de8954cc6d8b70e55b
tracer-ysyx <tracer@ysyx.org>
2025-10-26 01:17:00 +0800

    > sim RTL ysyx_25050136 谢平江 Linux yunhai 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:17:00 up 2 days, 12:21,  1 user,  load average: 1.17, 1.10, 1.14

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -200,7 +200,7 @@ module ysyx_25050136_MEM
             if(req_valid_o)begin
                 if(!((req_addr_o >= 32'h3000_0000 && req_addr_o < 32'h4000_0000) || 
                    (req_addr_o >= 32'h0f00_0000 && req_addr_o < 32'h1000_0000) ||
-                   (req_addr_o >= 32'ha000_0000 && req_addr_o < 32'hc000_0000)))
+                   (req_addr_o >= 32'ha000_0000 && req_addr_o < 32'hc000_0000)) || is_clint)
                     out_dbg_is_device_o <= 1;
             end else if(out_fire) begin
                     out_dbg_is_device_o <= 0;                
