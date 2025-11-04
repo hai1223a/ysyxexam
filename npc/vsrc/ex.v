20ff4074f56c0acae0df52de4562a62c12c9b5d1
tracer-ysyx <tracer@ysyx.org>
2025-11-04 17:00:41 +0800

    > sim RTL ysyx_25050136 谢平江 Linux WIN-PF6P3IV6POO 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:00:41 up  5:30,  1 user,  load average: 0.40, 0.22, 0.29

--- npc/vsrc/ex.v
+++ npc/vsrc/ex.v
@@ -53,6 +53,7 @@ module ysyx_25050136_EX
         output                                    branch_valid_o,
         output    [31:0]                            branch_npc_o,
         output    [ADDR_WIDTH-1:0]                       waddr_o
+        // output    [31:0]                                 wdata_o    
      );
     // ==== 信号定义 ====
     // 时序逻辑
@@ -202,6 +203,7 @@ module ysyx_25050136_EX
     assign out_rd_en_o = ex_rd_en;
     assign out_gpr_wdata_o = ex_csr_ren ? csru_out : alu_out;
     assign waddr_o = ex_rd & {ADDR_WIDTH{ex_rd_en & out_valid_o}};
+    assign wdata_o = out_gpr_wdata_o;
     // === 握手 ===
     assign ready_go = 1;
     assign in_ready_o = idle || out_fire;
