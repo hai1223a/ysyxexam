64e6f06803f46cf5b203c69d22658e38cc01475e
tracer-ysyx <tracer@ysyx.org>
2025-11-04 20:33:39 +0800

    > sim RTL ysyx_25050136 谢平江 Linux WIN-PF6P3IV6POO 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:33:39 up  9:03,  1 user,  load average: 0.12, 0.05, 0.01

--- npc/vsrc/icache_wrapper.v
+++ npc/vsrc/icache_wrapper.v
@@ -38,7 +38,7 @@ module ysyx_25050136_ICACHE_WRAPPER
     ysyx_25050136_ICACHE 
     #(
         .OFFSET_WIDTH 	(4             ),
-        .NUM_WAY      	(1             ),
+        .NUM_WAY      	(2             ),
         .INDEX_WIDTH  	(1             )
     )
     u_ysyx_25050136_ICACHE(
