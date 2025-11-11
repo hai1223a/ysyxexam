495e9b2e77aa871b1ee02b7a9033f2577794796b
tracer-ysyx <tracer@ysyx.org>
2025-11-12 00:01:34 +0800

    > sim RTL ysyx_25050136 谢平江 Linux WIN-PF6P3IV6POO 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:01:34 up 3 days, 10:56,  1 user,  load average: 0.10, 0.09, 0.09

--- npc/vsrc/bqu.v
+++ npc/vsrc/bqu.v
@@ -10,5 +10,5 @@ module ysyx_25050136_BQU
     wire [31:0] result;
     assign result_add = op1_i + op2_i;
     assign result = is_jalr_i ? {result_add[31:1],1'b0} : result_add[31:0];
-    assign out_o = 1;
+    assign out_o = result;
 endmodule
