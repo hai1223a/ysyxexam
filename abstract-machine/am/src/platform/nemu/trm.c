3b23c4cf86f311f5cc5fe7585164171f675c7bf5
tracer-ysyx <tracer@ysyx.org>
2025-11-21 14:12:25 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux WIN-PF6P3IV6POO 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:12:25 up 6 days, 22:28,  1 user,  load average: 0.27, 0.28, 0.43

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -7,10 +7,21 @@ int main(const char *args);
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
+int count = 0;
 void putch(char ch) {
-  outb(SERIAL_PORT, ch);
+  while(1) {
+    if(count < 16) {
+      outb(SERIAL_PORT, ch);
+      count++;
+      break;
+    }
+    if(inb(0x0f0000000)) {
+      outb(SERIAL_PORT, ch);
+      count = 0;
+      break;
+    } 
+  }
 }
-
 void halt(int code) {
   nemu_trap(code);
 
