16ee761d767f5d7b7b49eec71a763f70ff2a7380
tracer-ysyx <tracer@ysyx.org>
2025-11-21 14:18:13 +0800

    > compile NEMU ysyx_25050136 谢平江 Linux WIN-PF6P3IV6POO 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:18:13 up 6 days, 22:34,  1 user,  load average: 0.09, 0.17, 0.33

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -10,13 +10,13 @@ static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined 
 int count = 0;
 
 void putch(char ch) {
-  for(int i = 0;i < 100 ; i++) {
+  for(int i = 0;i < 1001 ; i++) {
     if(count < 16) {
       outb(SERIAL_PORT, ch);
       count++;
       break;
     }
-    if(i == 99) {
+    if(i == 1000) {
       outb(SERIAL_PORT, ch);
       count = 0;
       break;
