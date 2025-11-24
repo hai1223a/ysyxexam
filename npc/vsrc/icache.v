d41ccfea6bb69a4a786c5108db7dd4a1735783d1
tracer-ysyx <tracer@ysyx.org>
2025-11-24 13:45:53 +0800

    > sim RTL ysyx_25050136 谢平江 Linux WIN-PF6P3IV6POO 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:45:53 up 1 day,  3:19,  1 user,  load average: 1.32, 1.29, 0.70

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -203,4 +203,23 @@ module ysyx_25050136_ICACHE
     assign req_valid_o      = (state == MISS);
     assign req_addr_o     = ic_addr;
 
+`ifdef VERILATOR
+    reg [31:0] mem_type;
+    always @(posedge clk) begin
+        if(reset) begin
+
+        end else begin
+            if(state == MISS) begin
+                icache_misscycle(mem_type);
+            end 
+            if(flush) begin
+
+            end else if(in_fire) begin
+                if(ic_addr >= 32'ha0000000) begin mem_type <= 32'd2; icache_get(2); if(miss) icache_miss(2); end
+                else if(ic_addr >= 32'h30000000) begin mem_type <= 32'd1; icache_get(1); if(miss) icache_miss(1); end
+                else if(ic_addr >= 32'h0f000000) begin mem_type <= 32'd0; icache_get(0); if(miss) icache_miss(0); end
+            end
+        end
+    end
+`endif
 endmodule
