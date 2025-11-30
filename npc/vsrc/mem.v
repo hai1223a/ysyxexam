3dfafa8db2f838d72a628eb69f7fd5fde92c48e8
tracer-ysyx <tracer@ysyx.org>
2025-11-30 12:03:38 +0800

    > sim RTL ysyx_25050136 谢平江 Linux WIN-PF6P3IV6POO 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:03:38 up 13:40,  1 user,  load average: 0.26, 0.33, 0.41

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -210,7 +210,7 @@ module ysyx_25050136_MEM
         end else begin
             if((req_valid_o && !((req_addr_o >= 32'h3000_0000 && req_addr_o < 32'h4000_0000) || 
                    (req_addr_o >= 32'h0f00_0000 && req_addr_o < 32'h1000_0000) ||
-                   (req_addr_o >= 32'ha000_0000 && req_addr_o < 32'hc000_0000))) || is_clint) begin
+                   (req_addr_o >= 32'ha000_0000 && req_addr_o < 32'hc000_0000))) || (is_clint && in_req_ren_i)) begin
                     out_dbg_is_device_o <= 1;
             end else if(out_fire) begin
                     out_dbg_is_device_o <= 0;                
