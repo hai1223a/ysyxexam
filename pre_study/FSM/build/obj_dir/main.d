2adc521f09ffaed67e5e9c4f13e37fd2ad0cd7fc
ysyx_25050136-Xie pingjiang <2690477553@qq.com>
2025-12-01 12:17:35 +0800

    B阶段考核准备2

--- pre_study/FSM/build/obj_dir/main.d
+++
@@ -1,8 +0,0 @@
-main.o: /home/yunhai/ysyx-workbench/pre_study/FSM/csrc/main.cpp Vtop.h \
- /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated.h \
- /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilatedos.h \
- /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_config.h \
- /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_types.h \
- /home/yunhai/tools/oss-cad-suite/share/verilator/include/verilated_funcs.h \
- /home/yunhai/ysyx-workbench/nvboard/usr/include/nvboard.h \
- /home/yunhai/ysyx-workbench/nvboard/usr/include/pins.h
--- pre_study/cache/C/memory.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MEMORY_H__
-#define __MEMORY_H__
-
-#define MEMORY_SIZE (128 * 1024 * 1024)    // 128KB
-extern unsigned char physical_memory[MEMORY_SIZE];
-
-void write_file();
-void load_memory_from_file();
-#endif
