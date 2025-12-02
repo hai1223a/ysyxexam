194cc370d1348761f79dee935b8882b0e2c47797
ysyx_25050136-Xie pingjiang <2690477553@qq.com>
2025-12-02 16:39:39 +0800

    B阶段考核准备4

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -14,7 +14,8 @@ LDFLAGS   += --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 override NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt  \
                       -r $(shell dirname $(IMAGE).elf)/tracer.bin  \
-                      -k $(shell dirname $(IMAGE).elf)/nemu-dtracer-log.txt 
+                      -k $(shell dirname $(IMAGE).elf)/nemu-dtracer-log.txt \
+                      -b 
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
